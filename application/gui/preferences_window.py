d1d882399c2419e9cc6fdebe5dfcbf5c56cc9f41
MeanEYE.rcf <unknown>
2011-03-01 02:18:05 +0100

    Preferences tab for plugin management.

--- application/gui/preferences_window.py
+++ application/gui/preferences_window.py
@@ -53,6 +53,10 @@ class PreferencesWindow(gtk.Window):
 					ToolOptions(self, parent),
 					gtk.Label(_('Tools Menu'))
 					)
+		self._tabs.append_page(
+					PluginOptions(self, parent),
+					gtk.Label(_('Plugins'))
+					)
 
 		# create buttons
 		hbox = gtk.HBox(False, 2)
@@ -367,7 +371,7 @@ class ItemListOptions(gtk.VBox):
 			self._checkbox_shift.set_active(False)
 
 		# enable save button
-		self._parent.enable_save()
+		self._parent.enable_save(show_restart=True)
 
 	def _load_options(self):
 		"""Load item list options"""
@@ -703,7 +707,7 @@ class BookmarkOptions(gtk.VBox):
 
 
 class ToolOptions(gtk.VBox):
-	"""Bookmark options extension class"""
+	"""Tools options extension class"""
 
 	def __init__(self, parent, application):
 		gtk.VBox.__init__(self, False, 0)
@@ -857,7 +861,7 @@ class ToolOptions(gtk.VBox):
 
 
 class TerminalOptions(gtk.VBox):
-	"""Bookmark options extension class"""
+	"""Terminal options extension class"""
 
 	def __init__(self, parent, application):
 		gtk.VBox.__init__(self, False, 0)
@@ -888,3 +892,106 @@ class TerminalOptions(gtk.VBox):
 		_bool = ('False', 'True')
 
 		options.set('main', 'terminal_scrollbars', _bool[self._checkbox_scrollbars_visible.get_active()])
+
+
+class PluginOptions(gtk.VBox):
+	"""Plugins options extension class"""
+
+	def __init__(self, parent, application):
+		gtk.VBox.__init__(self, False, 0)
+
+		self._parent = parent
+		self._application = application
+
+		# configure self
+		self.set_border_width(10)
+		self.set_spacing(5)
+
+		# create interface
+		container = gtk.ScrolledWindow()
+		container.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_ALWAYS)
+		container.set_shadow_type(gtk.SHADOW_IN)
+
+		# create list box
+		self._plugins = gtk.ListStore(bool, str)
+
+		self._list = gtk.TreeView()
+		self._list.set_model(self._plugins)
+		self._list.set_rules_hint(True)
+
+		# create and configure cell renderers
+		cell_active = gtk.CellRendererToggle()
+		cell_active.connect('toggled', self._toggle_plugin)
+		cell_name = gtk.CellRendererText()
+
+		# create and pack columns
+		col_active = gtk.TreeViewColumn(_('Active'), cell_active, active=0)
+
+		col_name = gtk.TreeViewColumn(_('Plugin file'), cell_name, text=1)
+		col_name.set_resizable(True)
+		col_name.set_expand(True)
+
+		self._list.append_column(col_active)
+		self._list.append_column(col_name)
+
+		container.add(self._list)
+		self.pack_start(container, True, True, 0)
+
+	def _toggle_plugin(self, cell, path):
+		"""Handle changing plugin state"""
+		plugin = self._plugins[path][1]
+
+		if plugin not in self._application.protected_plugins:
+			# plugin is not protected, toggle it's state
+			self._plugins[path][0] = not self._plugins[path][0]
+
+			# enable save button
+			self._parent.enable_save(show_restart=True)
+
+		else:
+			# plugin is protected, show appropriate message
+			dialog = gtk.MessageDialog(
+									self._application,
+									gtk.DIALOG_DESTROY_WITH_PARENT,
+									gtk.MESSAGE_INFO,
+									gtk.BUTTONS_OK,
+									_(
+										"Specified plugin ('{0}') is required for "
+										"normal program operation and therefore can "
+										"not be deactivated!"
+									).format(plugin)
+								)
+
+			dialog.run()
+			dialog.destroy()
+
+	def _load_options(self):
+		"""Load terminal tab options"""
+		options = self._application.options
+
+		# clear existing list
+		self._plugins.clear()
+
+		# get list of plugins
+		list_ = self._application._get_plugin_list()
+		plugins_to_load = options.get('main', 'plugins').split(',')
+
+		# populate list
+		for plugin in list_:
+			self._plugins.append((
+							plugin in plugins_to_load,
+							plugin
+						))
+
+	def _save_options(self):
+		"""Save terminal tab options"""
+		options = self._application.options
+
+		# get only selected plugins
+		list_ = filter(lambda row: row[0], self._plugins)
+
+		# we need only plugin names
+		list_ = [row[1] for row in list_]
+
+		# save plugin list
+		options.set('main', 'plugins', ','.join(list_))

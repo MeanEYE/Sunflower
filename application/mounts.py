854dc165380eedd041fbb5d01137b56160596242
MeanEYE.rcf <unknown>
2011-05-27 23:29:52 +0200

    Mounts: Support for unmounting.

--- application/mounts.py
+++ application/mounts.py
@@ -14,15 +14,22 @@ class MountsManager:
 		self._application = application
 		self._menu_item = menu_item
 
+		menu_manager = self._application.menu_manager
+
 		# create mounts menu
 		self._menu = gtk.Menu()
 		self._menu_item.set_submenu(self._menu)
+
+		self._menu_unmount = menu_manager.get_item_by_name('unmount_menu').get_submenu()
 
 		# create item for usage when there are no mounts
 		self._menu_item_no_mounts = gtk.MenuItem(label=_('Mount list is empty'))
 		self._menu_item_no_mounts.set_sensitive(False)
 		self._menu_item_no_mounts.set_property('no-show-all', True)
 		self._menu.append(self._menu_item_no_mounts)
+
+		self._menu_item_no_mounts2 = menu_manager.get_item_by_name('mount_list_empty')
+		self._menu_item_no_mounts2.set_property('no-show-all', True)
 
 		# gnome volume monitor
 		self._volume_monitor = VolumeMonitor()
@@ -40,11 +47,19 @@ class MountsManager:
 		icon_names = mount.get_icon().to_string()
 		mount_icon = self._application.icon_manager.get_mount_icon_name(icon_names)
 
+		# add bookmark menu item
 		self._add_item(
-	            mount.get_name(),
-	            mount.get_root().get_path(),
-	            mount_icon
-	        )
+				mount.get_name(),
+				mount.get_root().get_path(),
+				mount_icon
+			)
+
+		# add unmount menu item
+		self._add_unmount_item(
+				mount.get_name(),
+				mount.get_root().get_path(),
+				mount_icon
+			)
 
 		# notify system about menu update
 		self._menu_updated()
@@ -60,6 +75,7 @@ class MountsManager:
 		"""Method called whenever menu is updated"""
 		has_mounts = len(self._menu.get_children()) > 1
 		self._menu_item_no_mounts.set_visible(not has_mounts)
+		self._menu_item_no_mounts2.set_visible(not has_mounts)
 
 	def _add_item(self, text, path, icon):
 		"""Add new menu item to the list"""
@@ -75,7 +91,61 @@ class MountsManager:
 
 		self._menu.append(menu_item)
 
+	def _add_unmount_item(self, text, path, icon):
+		"""Add new menu item used for unmounting"""
+		image = gtk.Image()
+		image.set_from_icon_name(icon, gtk.ICON_SIZE_MENU)
+
+		menu_item = gtk.ImageMenuItem()
+		menu_item.set_label(text)
+		menu_item.set_image(image)
+		menu_item.set_data('path', path)
+		menu_item.connect('activate', self._unmount_item)
+		menu_item.show()
+
+		self._menu_unmount.append(menu_item)
+
 	def _remove_item(self, mount_point):
 		"""Remove item based on device name"""
 		for item in self._menu.get_children():
 			if item.get_data('path') == mount_point: self._menu.remove(item)
+
+		for item in self._menu_unmount.get_children():
+			if item.get_data('path') == mount_point: self._menu_unmount.remove(item)
+
+	def _unmount_item(self, widget, data=None):
+		"""Unmount item"""
+		path = widget.get_data('path')
+
+		for mount in self._volume_monitor.get_mounts():
+			# check if this is the right mount
+			if mount.get_root().get_path() == path:
+				self._unmount(mount)
+				break
+
+	def _unmount(self, mount):
+		"""Perform unmounting"""
+		if mount.can_unmount():
+			# we can safely unmount
+			mount.unmount(self._unmount_finish)
+
+		else:
+			# print error
+			dialog = gtk.MessageDialog(
+									self,
+									gtk.DIALOG_DESTROY_WITH_PARENT,
+									gtk.MESSAGE_WARNING,
+									gtk.BUTTONS_OK,
+									_("Specified item can not be unmounted. ")
+								)
+			dialog.run()
+			dialog.destroy()
+
+	def _unmount_finish(self, mount, result):
+		"""Callback for unmount events"""
+		try:
+			# try to finish async unmount
+			mount.unmount_finish(result)
+
+		except:
+			pass

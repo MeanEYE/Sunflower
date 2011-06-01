ad51663955d7e7cad40649b0ae8ca1e19551db16
MeanEYE.rcf <unknown>
2011-06-01 22:40:04 +0200

    Created base class for page in preferences window.

---
+++ application/widgets/settings_page.py
@@ -0,0 +1,28 @@
+import gtk
+
+
+class SettingsPage(gtk.VBox):
+	"""Abstract class used to build pages in preferences window."""
+	
+	def __init__(self, parent, application, name, title):
+		gtk.VBox.__init__(self, False, 0)
+
+		self._parent = parent
+		self._application = application
+		self._page_name = name
+		self._page_title = title
+
+		# configure self
+		self.set_spacing(5)
+		self.set_border_width(0)
+		
+		# add page to preferences window
+		self._parent.add_tab(self._page_title, self)
+		
+	def _load_options(self):
+		"""Load options and update interface"""
+		pass
+
+	def _save_options(self):
+		"""Method called when save button is clicked"""
+		pass

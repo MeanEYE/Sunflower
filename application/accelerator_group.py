d3e9218c5cfe7251432e7487e2087ba658cd8efb
MeanEYE.rcf <unknown>
2011-06-12 14:45:09 +0200

    Additional work on accelerator groups and manager.

--- application/accelerator_group.py
+++ application/accelerator_group.py
@@ -1,88 +1,152 @@
 import gtk
 
 
-class InvalidMethodError(Exception): pass
-
 class AcceleratorGroup:
-	"""Accelerator group is used to provide users with customizable
-	keyboard shortcuts for plugins.
-
-	This class will automatically provide configuration interface.
-
-	"""
+	"""Accelerator group provides customizable keyboard shortcuts for plugins
+	with automatically generated configuration."""
 
 	def __init__(self, application):
+		self._application = application
+		self._manager = self._application.accelerator_manager
+		self._active = False
+
 		self._name = None
 		self._title = None
+		self._window = None
 
+		# accelerator containers
 		self._methods = {}
+		self._primary = {}
+		self._secondary = {}
+		self._disabled = []
+
+		# method name cache
 		self._method_names = {}
-		self._method_accels = {}
 
-		self._accels = gtk.AccelGroup()
+		# accelerator implementation
+		self._accel_group = None
 
-	def _handle_accelerator_activate(self, group, widget, keyval, modifier):
+	def _register_group(self):
+		"""Register group with manager"""
+		self._manager.register_group(self)
+
+	def _create_group(self):
+		"""Create group and connect accelerators"""
+		self._accel_group = gtk.AccelGroup()
+
+		# create accelerators
+		self._create_accelerators()
+		self._create_accelerators(primary=False)
+
+		# register group with manager
+		self._register_group()
+
+	def _create_accelerators(self, primary=True):
+		"""Create accelerators from specified list"""
+		list_ = self._primary if primary else self._secondary
+
+		# connect all methods in list
+		for method_name in list_:
+			if method_name in self._disabled:
+				continue  # skip if method is disabled
+
+			# try to get saved key combination from manager
+			accelerator = self._manager.get_accelerator(self._name, method_name, primary)
+
+			# if we don't have saved key combination, use default
+			if accelerator is None:
+				accelerator = list_[method_name]
+
+			keyval = accelerator[0]
+			modifier = accelerator[1]
+
+			# create method name cache based on key combination
+			label = gtk.accelerator_get_label(keyval, modifier)
+			self._method_names[label] = method_name
+
+			# connect accelerator
+			self._accel_group.connect_group(keyval, modifier, 0, self._handle_activate)
+
+	def _handle_activate(self, group, widget, keyval, modifier):
 		"""Handle accelerator activation"""
 		label = gtk.accelerator_get_label(keyval, modifier)
 		name = self._method_names[label]
 
 		# call user method
-		result = self._methods[name]['callback'](widget, keyval)
+		return self._methods[name]['callback'](widget, label)
 
-		return result
+	def activate(self, window):
+		"""Activate accelerator group for specified window"""
+		self._window = window
+
+		# connect accelerators if they are not already
+		if self._accel_group is None:
+			self._create_group()
+
+		# add accelerator group to specified window
+		self._window.add_accel_group(self._accel_group)
+		self._active = True
+
+	def deactivate(self):
+		"""Deactivate accelerator group"""
+		if self._active:
+			self._window.remove_accel_group(self._accel_group)
+			self._active = False
+
+	def invalidate(self):
+		"""Force reloading accelerators"""
+		pass
 
 	def set_name(self, name):
-		"""Set accelerator group name
-
-		This name will be used in config files.
-
-		"""
+		"""Set accelerator group name"""
 		self._name = name.replace(' ', '_')
 
 	def set_title(self, title):
-		"""Set accelerator group title
-
-		This title will be used in configuration page.
-
-		"""
+		"""Set accelerator group title"""
 		self._title = title
 
 	def add_method(self, name, title, callback):
-		"""Add accelerator to group"""
+		"""Add new method to group"""
 		self._methods[name] = {
 						'title': title,
 						'callback': callback
 					}
 
 	def set_accelerator(self, name, keyval, modifier):
-		"""Set accelerator for specified method name
+		"""Set primary accelerator for specified method name"""
+		self._primary[name] = (keyval, modifier)
 
-		key - Integer value for a key
-		mods - Modifier mask (eg. gtk.gdk.CONTROL_MASK | gtk.gdk.SHIFT_MASK)
+	def set_alt_accelerator(self, name, keyval, modifier):
+		"""Set secondary accelerator for specified method name"""
+		self._secondary[name] = (keyval, modifier)
 
-		"""
-		if not self._methods.has_key:
-			raise InvalidMethodError('Specified method name ({0}) is not valid!'.format(name))
+	def reset_accelerator(self, name):
+		"""Resets accelerator shortcuts"""
+		if self._primary.has_key(name):
+			del self._primary[name]
 
-		# connect method name to accelerator label
-		label = gtk.accelerator_get_label(keyval, modifier)
-		self._method_names[label] = name
+		if self._secondary.has_key(name):
+			del self._secondary[name]
 
-		# connect accelerator to local method
-		self._accels.connect_group(keyval, modifier, 0, self._handle_accelerator_activate)
+		# remove any cache
+		for label in self._method_names:
+			if self._method_names[label] == name:
+				del self._method_names[label]
 
-	def get_accel_group(self):
-		"""Return GTK+ accel group"""
-		return self._accels
+	def disable_accelerator(self, name):
+		"""Disable specified accelerator"""
+		self._disabled.append(name)
 
-	def trigger_accelerator(self, widget, key, mods):
+	def trigger_accelerator(self, keyval, modifier):
 		"""Manually trigger accelerator"""
 		result = False
-		default_mods = gtk.accelerator_get_default_mod_mask()  # filter out unneeded mods
-		label = gtk.accelerator_get_label(key, mods & default_mods)
+
+		modifier = modifier & gtk.accelerator_get_default_mod_mask() # filter out unneeded mods
+		label = gtk.accelerator_get_label(keyval, modifier)
 
 		# trigger accelerator only if we have method connected
 		if self._method_names.has_key(label):
-			result = self._handle_accelerator_activate(self._accels, widget, key, mods & default_mods)
+			result = self._handle_activate(self._accel_group, self._window, keyval, modifier)
 
 		return result
+
--- application/accelerator_manager.py
+++ application/accelerator_manager.py
@@ -1,16 +1,64 @@
+import gtk
 
 
 class AcceleratorManager:
 
 	def __init__(self, application):
 		self._application = application
+		self._config = None
 
 		self._groups = []
 		self._group_names = []
 
+	def _save_accelerator(self, section, name, accelerator=None, primary=True, can_overwrite=False):
+		"""Save accelerator to config file"""
+		if not primary:
+			name = '{0}_2'.format(name)
+
+		label = ''
+		if accelerator is not None:
+			label = gtk.accelerator_name(*accelerator)
+
+		# don't allow overwriting user's configuration unless strictly specified
+		if not self._config.has_option(section, name) \
+		or (self._config.has_option(section, name) and can_overwrite):
+			self._config.set(section, name, label)
+
+	def _load_accelerator(self, section, name, primary=True):
+		"""Load accelerator from config file"""
+		result = None
+
+		if not primary:
+			name = '{0}_2'.format(name)
+
+		# try to load only if config has accelerator specified
+		if self._config.has_section(section) \
+		and self._config.has_option(section, name):
+			accelerator = gtk.accelerator_parse(self._config.get(section, name))
+
+			if gtk.accelerator_valid(*accelerator):
+				result = accelerator
+
+		return result
+
 	def register_group(self, group):
 		"""Register group with manager"""
-		pass
+		if not self._config.has_section(group._name):
+			self._config.add_section(group._name)
+
+		# add all the methods to config file
+		for name in group._methods:
+
+			# save primary accelerator
+			if group._primary.has_key(name):
+				self._save_accelerator(group._name, name, group._primary[name])
+
+			else:
+				self._save_accelerator(group._name, name)
+
+			# save secondary accelerator
+			if group._secondary.has_key(name):
+				self._save_accelerator(group._name, name, group._secondary[name], primary=False)
 
 	def get_groups(self):
 		"""Get list of unique group names"""
@@ -22,14 +70,12 @@ class AcceleratorManager:
 
 	def get_accelerator(self, group, name, primary=True):
 		"""Get saved accelerator"""
-		result = None
+		return self._load_accelerator(group, name, primary)
 
-		return result
-
-	def save_accelerators(self):
-		"""Save accelerator map"""
-		pass
-
-	def load_accelerators(self):
+	def load(self, config):
 		"""Load accelerator map"""
+		self._config = config
+
+	def save(self):
+		"""Save accelerator map"""
 		pass

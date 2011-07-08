91f119ebd3f90a5089e7bad1d06f6bdf1acb42b8
MeanEYE.rcf <unknown>
2011-07-08 22:38:08 +0200

    Rename extension: Added get_title method.

--- application/plugin_base/rename_extension.py
+++ application/plugin_base/rename_extension.py
@@ -19,7 +19,7 @@ class RenameExtension(gtk.VBox):
 		# create activity toggle
 		self._active = False
 		self._checkbox_active = gtk.CheckButton(_('Use this extension'))
-		self._checkbox_active.connect('toggle', self.__toggle_active)
+		self._checkbox_active.connect('toggled', self.__toggle_active)
 		self._checkbox_active.show()
 		
 		self.pack_start(self._checkbox_active, False, False, 0)
@@ -27,6 +27,10 @@ class RenameExtension(gtk.VBox):
 	def __toggle_active(self, widget, data=None):
 		"""Toggle extension active property"""
 		self._active = widget.get_active()
+		
+	def get_title(self):
+		"""Return i18n title for extension"""
+		return None
 		
 	def get_new_name(self, file_name, old_name):
 		"""Generate and return new name for specified file.

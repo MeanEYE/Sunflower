bff53972032591ffcd2160d28d6856c1030df9cd
Wojciech Kluczka <wojtekkluczka@gmail.com>
2011-07-12 23:59:13 +0200

    Rename Extensions: letter case extension added

--- application/plugins/rename_extensions.py
+++ application/plugins/rename_extensions.py
@@ -1,6 +1,7 @@
 import re
 import os
 import gtk
+import string
 
 from plugin_base.rename_extension import RenameExtension
 from gui.input_dialog import InputRangeDialog
@@ -10,6 +11,7 @@ from tools.advanced_rename import Column as RenameColumn
 def register_plugin(application):
 	"""Register plugin classes with application"""
 	application.register_rename_extension('default', DefaultRename)
+	application.register_rename_extension('lettercase', LetterCaseRename)
 	
 
 class DefaultRename(RenameExtension):
@@ -311,4 +313,81 @@ class DefaultRename(RenameExtension):
 		self._counter += self._counter_step
 		
 		return result
+		
+		
+class LetterCaseRename(RenameExtension):
+	"""Letter case rename extension support"""
+	
+	def __init__(self, parent):
+		super(LetterCaseRename, self).__init__(parent)
+		
+		self._basename_methods = (
+					(_('Do nothing'), self.__do_nothig),
+					(_('Capitalize'), self.__capitalize),
+					(_('Upper case'), self.__upper),
+					(_('Lower case'), self.__lower),
+					(_('Swap case'), self.__swap),
+				)
+
+		self._extension_methods = (
+					(_('Do nothing'), self.__do_nothig),
+					(_('Upper case'), self.__upper),
+					(_('Lower case'), self.__lower),
+				)
+
+		# fill combos
+		self._combo_basename = gtk.combo_box_new_text()
+		for method in self._basename_methods:
+			self._combo_basename.append_text(method[0])
+		self._combo_basename.set_active(0)
+		self._combo_basename.connect('changed', self._update_parent_list)
+
+		self._combo_extension = gtk.combo_box_new_text()
+		for method in self._extension_methods:
+			self._combo_extension.append_text(method[0])
+		self._combo_extension.set_active(0)
+		self._combo_extension.connect('changed', self._update_parent_list)
+
+		label_basename = gtk.Label(_('Besename:'))
+		label_extension = gtk.Label(_('Extension:'))
+
+		# pack gui
+		table = gtk.Table(2, 2)
+		table.attach(label_basename, 0, 1, 0, 1, xpadding=4, xoptions=(not gtk.EXPAND))
+		table.attach(label_extension, 0, 1, 1, 2, xpadding=4, xoptions=(not gtk.EXPAND))
+		table.attach(self._combo_basename, 1, 2, 0, 1)
+		table.attach(self._combo_extension, 1, 2, 1, 2)
+
+		self.pack_start(table, False, False, 0)
+
+	def __do_nothig(self, name):
+		"""Return the same string"""
+		return name
+
+	def __capitalize(self, name):
+		"""Return capitalized string"""
+		return string.capwords(name)
+		
+	def __upper(self, name):
+		"""Return upper case string"""
+		return name.upper()
+		
+	def __lower(self, name):
+		"""Return lower case string"""
+		return name.lower()
+		
+	def __swap(self, name):
+		"""Swap case in string"""
+		return name.swapcase()
+				
+	def get_title(self):
+		"""Return extension title"""
+		return _('Letter Case')
+	
+	def get_new_name(self, old_name, new_name):
+		"""Get modified name"""
+		basename, extension = os.path.splitext(new_name)		
+		new_basename = self._basename_methods[self._combo_basename.get_active()][1](basename)
+		new_extension = self._extension_methods[self._combo_extension.get_active()][1](extension)
+		return "{0}{1}".format(new_basename, new_extension)
 		

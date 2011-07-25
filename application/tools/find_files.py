2b407e4e4aca093fb879dfdae5728bd16c8aefea
MeanEYE.rcf <unknown>
2011-07-25 18:52:20 +0200

    Tools: Find files: Additional work on GUI.

--- application/tools/find_files.py
+++ application/tools/find_files.py
@@ -1,5 +1,6 @@
 import os
 import gtk
+import user
 
 
 class FindFiles(gtk.Window):
@@ -16,7 +17,7 @@ class FindFiles(gtk.Window):
 		
 		# configure window
 		self.set_title(_('Find files'))
-		self.set_default_size(640, 600)
+		self.set_default_size(550, 500)
 		self.set_position(gtk.WIN_POS_CENTER_ON_PARENT)
 		self.set_transient_for(application)
 		self.set_border_width(7)
@@ -24,4 +25,56 @@ class FindFiles(gtk.Window):
 		
 		# create interface
 		vbox = gtk.VBox(False, 7)
+
+		# create modifiers notebook
+		self._extension_list = gtk.Notebook()
+
+		# create controls
+		hbox_controls = gtk.HBox(False, 5)
+
+		button_find = gtk.Button(stock=gtk.STOCK_FIND)
+		button_find.connect('clicked', self.find_files)
+
+		button_stop = gtk.Button(stock=gtk.STOCK_STOP)
+		button_stop.set_sensitive(False)
+		button_stop.connect('clicked', self.stop_search)
+
+		button_cancel = gtk.Button(stock=gtk.STOCK_CANCEL)
+		button_cancel.connect('clicked', self._close_window)
 		
+		# pack interface
+		hbox_controls.pack_end(button_find, False, False, 0)
+		hbox_controls.pack_end(button_stop, False, False, 0)
+		hbox_controls.pack_end(button_cancel, False, False, 0)
+
+		vbox.pack_end(self._extension_list, False, False, 0) 
+		vbox.pack_end(hbox_controls, False, False, 0)
+
+		self.add(vbox)
+
+		# show all widgets
+		self.show_all()
+
+	def __create_extensions(self):
+		"""Create rename extensions"""
+		for ExtensionClass in self._application.rename_extension_classes.values():
+			extension = ExtensionClass(self)
+			title = extension.get_title()
+		
+			# add tab	
+			self._extension_list.append_page(extension, gtk.Label(title))
+			
+			# store extension for later use
+			self._extensions.append(extension)
+
+	def _close_window(self, widget=None, data=None):
+		"""Close window"""
+		self.destroy()
+
+	def stop_search(self, widget=None, data=None):
+		"""Stop searching for files"""
+		pass
+
+	def find_files(self, widget=None, data=None):
+		"""Start searching for files"""
+		pass

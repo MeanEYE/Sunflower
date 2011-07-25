2e99681a90b0635dc2f8ec93f6f375927b0c4119
MeanEYE.rcf <unknown>
2011-07-25 19:56:54 +0200

    Plugins: Created default find file extensions plugin.

---
+++ application/plugins/find_file_extensions.py
@@ -0,0 +1,24 @@
+from plugin_base.find_extension import FindExtension
+
+
+def register_plugin(application):
+	"""register plugin classes with application"""
+	application.register_find_extension('default', DefaultFindFiles)
+
+
+class DefaultFindFiles(FindExtension):
+	"""Default extension for find files tool"""
+
+	def __init__(self, parent):
+		super(DefaultFindFiles, self).__init__(parent)
+
+		# remove "active" checkbox
+
+
+	def get_title(self):
+		"""Return i18n title for extension"""
+		return _('Basic')
+
+	def is_file_ok(self, path):
+		"""Check is specified path fits the cirteria"""
+		return True

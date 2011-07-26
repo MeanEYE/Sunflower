7408ec7d37a2ee143d6f8b55ff142fe9cd5db9ba
MeanEYE.rcf <unknown>
2011-07-26 19:53:50 +0200

    Preferences: Accelerators: Created list and method to populate it.

--- application/gui/preferences/accelerators.py
+++ application/gui/preferences/accelerators.py
@@ -2,11 +2,13 @@ import gtk
 
 from widgets.settings_page import SettingsPage
 
-COL_NAME			= 0
-COL_PRIMARY_KEY		= 1
-COL_PRIMARY_MODS	= 2
-COL_SECONDARY_KEY	= 3
-COL_SECONDARY_MODS	= 4
+
+class Column:
+	NAME = 0
+	PRIMARY_KEY = 1
+	PRIMARY_MODS = 2
+	SECONDARY_KEY = 3
+	SECONDARY_MODS = 4
 
 
 class AcceleratorOptions(SettingsPage):
@@ -32,23 +34,23 @@ class AcceleratorOptions(SettingsPage):
 		cell_secondary = gtk.CellRendererAccel()
 
 		# create and pack columns
-		col_name = gtk.TreeViewColumn('Test', cell_name, text=COL_NAME)
+		col_name = gtk.TreeViewColumn(_('Description'), cell_name, markup=Column.NAME)
 		col_name.set_min_width(200)
 		col_name.set_resizable(True)
 
 		col_primary = gtk.TreeViewColumn(
-									'Test',
+									_('Primary'),
 									cell_primary,
-									accel_key=COL_PRIMARY_KEY,
-									accel_mods=COL_PRIMARY_MODS
+									accel_key=Column.PRIMARY_KEY,
+									accel_mods=Column.PRIMARY_MODS
 								)
 		col_primary.set_min_width(100)
 
 		col_secondary = gtk.TreeViewColumn(
-									'Test',
+									_('Secondary'),
 									cell_secondary,
-									accel_key=COL_SECONDARY_KEY,
-									accel_mods=COL_SECONDARY_MODS
+									accel_key=Column.SECONDARY_KEY,
+									accel_mods=Column.SECONDARY_MODS
 								)
 		col_secondary.set_min_width(100)
 
@@ -60,3 +62,73 @@ class AcceleratorOptions(SettingsPage):
 		container.add(self._list)
 
 		self.pack_start(container, True, True, 0)
+
+	def _populate_list(self):
+		"""Update accelerator list"""
+		manager = self._application.accelerator_manager
+		bookmarks = self._application.bookmark_options
+		groups = manager.get_groups()
+		groups.sort()
+
+		# clear accelerator list
+		self._accels.clear()
+
+		# create rename list
+		replace_list = {}
+
+		# add bookmarks to the replace list
+		for number in range(1, 11):
+			key_name = '{0}.{1}_{2}'.format('item_list', 'bookmark', number)
+			bookmark_name = 'b_{0}'.format(number)
+
+			if bookmarks.has_option('bookmarks', bookmark_name):
+				# bookmark exists
+				data = bookmarks.get('bookmarks', bookmark_name).split(';', 1)
+				bookmark_value = data[0]
+
+			else:
+				# bookmark doesn't exist, add generic name
+				bookmark_value = 'Bookmark #{0}'.format(number)
+
+			replace_list[key_name] = bookmark_value
+
+		# add methods
+		for group_name in groups:
+			title, methods = manager.get_group_data(group_name)
+
+			method_names = methods.keys()
+			method_names.sort()
+
+			# add group and save iter for later use
+			group_iter = self._accels.append(None, ('<i>{0}</i>'.format(title), 0, 0, 0 ,0))
+
+			for method_name in method_names:
+				# add all methods from the group
+				title = methods[method_name]['title']
+
+				# check if specified method name has a rename value
+				key_name = '{0}.{1}'.format(group_name, method_name)
+				if replace_list.has_key(key_name):
+					title = title.format(replace_list[key_name])
+
+				# get accelerators
+				primary = manager.get_accelerator(group_name, method_name, True)
+				secondary = manager.get_accelerator(group_name, method_name, False)
+
+				# make sure we have something to display
+				if primary is None:
+					primary = (0, 0)
+
+				if secondary is None:
+					secondary = (0, 0)
+
+				# append to the list
+				self._accels.append(group_iter, (title, primary[0], primary[1], secondary[0], secondary[1]))
+
+	def _load_options(self):
+		"""Load options and update interface"""
+		self._populate_list()
+
+	def _save_options(self):
+		"""Method called when save button is clicked"""
+		pass

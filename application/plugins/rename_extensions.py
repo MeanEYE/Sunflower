807c178dc83316eb0b18ce46e9ee77abe13c86bc
MeanEYE.rcf <unknown>
2011-07-31 14:04:28 +0200

    Plugins: Rename extensions: Fixed problem with positioning "add" menu.

--- application/plugins/rename_extensions.py
+++ application/plugins/rename_extensions.py
@@ -200,8 +200,8 @@ class DefaultRename(RenameExtension):
 	def __get_menu_position(self, menu, button):
 		"""Get history menu position"""
 		# get coordinates
-		window_x, window_y = self._parent.window.get_position()
-		button_x, button_y = button.translate_coordinates(self._parent, 0, 0)
+		window_x, window_y = self._parent.window.window.get_position()
+		button_x, button_y = button.translate_coordinates(self._parent.window, 0, 0)
 		button_h = button.get_allocation().height
 
 		# calculate absolute menu position

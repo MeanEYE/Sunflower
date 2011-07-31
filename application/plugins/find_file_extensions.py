dfb75e99dbc16b0b93a223d9d977f51548524e7e
MeanEYE.rcf <unknown>
2011-07-31 14:57:40 +0200

    Plugins: Find file extensions: Created interface for default extension.

--- application/plugins/find_file_extensions.py
+++ application/plugins/find_file_extensions.py
@@ -1,3 +1,5 @@
+import gtk
+
 from plugin_base.find_extension import FindExtension
 
 
@@ -14,6 +16,52 @@ class DefaultFindFiles(FindExtension):
 	
 		# enabled by default
 		self._checkbox_active.set_active(True)
+
+		# help
+		label_help = gtk.Label()
+		label_help.set_alignment(0, 0)
+		label_help.set_use_markup(True)
+
+		label_help.set_markup(_(
+							'<b>Pattern matching</b>\n'
+							'*\t\tEverything\n'
+							'?\t\tAny single character\n'
+							'[seq]\tAny character in <i>seq</i>\n'
+							'[!seq]\tAny character <u>not</u> in <i>seq</i>'
+						))
+
+		# create containers
+		hbox = gtk.HBox(True, 15)
+		vbox_left = gtk.VBox(False, 5)
+		vbox_right = gtk.VBox(False, 0)
+
+		# create interface
+		vbox_entry = gtk.VBox(False, 0)
+
+		label_entry = gtk.Label(_('Search for:'))
+		label_entry.set_alignment(0, 0.5)
+		
+		self._entry = gtk.Entry()
+		self._entry.set_text('*')
+
+		self._checkbox_case_sensitive = gtk.CheckButton(_('Case sensitive'))
+
+		# pack interface
+		self.vbox.remove(self._checkbox_active)
+
+		vbox_entry.pack_start(label_entry, False, False, 0)
+		vbox_entry.pack_start(self._entry, False, False, 0)
+
+		vbox_left.pack_start(self._checkbox_active, False, False, 0)
+		vbox_left.pack_start(vbox_entry, False, False, 0)
+		vbox_left.pack_start(self._checkbox_case_sensitive, False, False, 0)
+
+		vbox_right.pack_start(label_help, True, True, 0)
+
+		hbox.pack_start(vbox_left, True, True, 0)
+		hbox.pack_start(vbox_right, True, True, 0)
+
+		self.vbox.pack_start(hbox, True, True, 0)
 
 	def get_title(self):
 		"""Return i18n title for extension"""

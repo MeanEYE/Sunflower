fd201ebab11801e33036c9d0fdbaa84bb40ac585
MeanEYE.rcf <unknown>
2011-10-10 10:01:14 +0200

    Preferences: Plugins: Changed the way locale was detected.

--- application/gui/preferences/plugins.py
+++ application/gui/preferences/plugins.py
@@ -1,5 +1,6 @@
 import os
 import gtk
+import locale
 
 from ConfigParser import ConfigParser
 from widgets.settings_page import SettingsPage
@@ -13,7 +14,7 @@ class Column:
 	VERSION = 4
 	CONTACT = 5
 	SITE = 6
-	
+
 
 class Section:
 	NAME = 'Name'
@@ -37,7 +38,7 @@ class PluginsOptions(SettingsPage):
 
 		# create list box
 		self._plugins = gtk.ListStore(
-									bool,	# active 
+									bool,	# active
 									str,	# location
 									str,	# name
 									str,	# author
@@ -64,32 +65,32 @@ class PluginsOptions(SettingsPage):
 		col_name = gtk.TreeViewColumn(_('Plugin name'), cell_name, text=Column.NAME)
 		col_name.set_resizable(True)
 		col_name.set_expand(True)
-		
+
 		col_author = gtk.TreeViewColumn(_('Author'), cell_author, text=Column.AUTHOR)
 		col_author.set_resizable(True)
-		
+
 		col_version = gtk.TreeViewColumn(_('Version'), cell_version, text=Column.VERSION)
 
 		self._list.append_column(col_active)
 		self._list.append_column(col_name)
 		self._list.append_column(col_author)
 		self._list.append_column(col_version)
-		
+
 		# create controls
 		hbox_controls = gtk.HBox(False, 5)
 
 		image_contact = gtk.Image()
 		image_contact.set_from_icon_name('gnome-stock-mail-new', gtk.ICON_SIZE_BUTTON)
-				
+
 		self._button_contact = gtk.Button()
 		self._button_contact.set_label(_('Contact'))
 		self._button_contact.set_image(image_contact)
 		self._button_contact.set_sensitive(False)
 		self._button_contact.connect('clicked', self.__handle_contact_button_click)
-		
+
 		image_home_page = gtk.Image()
 		image_home_page.set_from_stock(gtk.STOCK_HOME, gtk.ICON_SIZE_BUTTON)
-		
+
 		self._button_home_page = gtk.Button()
 		self._button_home_page.set_label(_('Visit site'))
 		self._button_home_page.set_image(image_home_page)
@@ -98,41 +99,41 @@ class PluginsOptions(SettingsPage):
 
 		# pack containers
 		container.add(self._list)
-		
+
 		hbox_controls.pack_start(self._button_contact, False, False, 0)
 		hbox_controls.pack_start(self._button_home_page, False, False, 0)
-		
+
 		self.pack_start(container, True, True, 0)
 		self.pack_start(hbox_controls, False, False, 0)
-		
+
 	def __handle_cursor_change(self, widget, data=None):
 		"""Update button state when list cursor changes"""
 		selection = widget.get_selection()
 		item_store, selected_iter = selection.get_selected()
-		
+
 		if selected_iter is not None:
 			has_contact = item_store.get_value(selected_iter, Column.CONTACT) is not None
 			has_site = item_store.get_value(selected_iter, Column.SITE) is not None
-			
+
 			self._button_contact.set_sensitive(has_contact)
 			self._button_home_page.set_sensitive(has_site)
-			
+
 	def __handle_contact_button_click(self, widget, data=None):
 		"""Create new contact email"""
 		selection = self._list.get_selection()
 		item_store, selected_iter = selection.get_selected()
-		
+
 		if selected_iter is not None:
 			email = item_store.get_value(selected_iter, Column.CONTACT)
 			email = 'gnome-open mailto:{0}'.format(email)
-			
+
 			os.system(email)
-			
+
 	def __handle_home_page_button_click(self, widget, data=None):
 		"""Create new contact email"""
 		selection = self._list.get_selection()
 		item_store, selected_iter = selection.get_selected()
-		
+
 		if selected_iter is not None:
 			url = item_store.get_value(selected_iter, Column.SITE)
 			self._application.goto_web(self, url)
@@ -177,13 +178,10 @@ class PluginsOptions(SettingsPage):
 		plugin_path = os.path.abspath(os.path.join('application', 'plugins'))
 		plugin_list = self._application._get_plugin_list()
 		plugins_to_load = options.get('main', 'plugins').split(',')
-		
+
 		# extract current locale
-		language = os.environ['LANGUAGE']
-		
-		if language.find(':') > -1:
-			language = language.split(':')[1]
-			
+		language = localge.getdefaultlocale()[0]
+
 		# populate list
 		for plugin in plugin_list:
 			# default values
@@ -194,21 +192,21 @@ class PluginsOptions(SettingsPage):
 			plugin_contact = None
 
 			plugin_config_file = os.path.join(plugin_path, plugin, 'plugin.conf')
-			
+
 			# read plugin data from configuration file
 			if os.path.exists(plugin_config_file):
 				config = ConfigParser()
 				config.read(plugin_config_file)
-				
+
 				if config.has_section(Section.NAME):
 					if config.has_option(Section.NAME, language):
-						# try to get plugin name for current language 
+						# try to get plugin name for current language
 						plugin_name = config.get(Section.NAME, language)
-					
+
 					elif config.has_option(Section.NAME, 'en'):
 						# try to get plugin name for default language
 						plugin_name = config.get(Section.NAME, 'en')
-					
+
 				if config.has_section(Section.AUTHOR):
 					# get author name
 					if config.has_option(Section.AUTHOR, 'name'):
@@ -220,7 +218,7 @@ class PluginsOptions(SettingsPage):
 
 					if config.has_option(Section.AUTHOR, 'site'):
 						plugin_site = config.get(Section.AUTHOR, 'site')
-			
+
 			# add plugin data to list
 			self._plugins.append((
 							plugin in plugins_to_load,
@@ -231,7 +229,7 @@ class PluginsOptions(SettingsPage):
 							plugin_contact,
 							plugin_site
 						))
-			
+
 	def _save_options(self):
 		"""Save terminal tab options"""
 		options = self._application.options

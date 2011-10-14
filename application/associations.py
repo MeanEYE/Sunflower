708bb8a62e9b01fdfbf9a00ac03070285451b244
MeanEYE.rcf <meaneye.rcf@gmail.com>
2011-10-14 17:12:54 +0200

    Associations manager: Use GIO instead of GnomeVFS to get application info.

--- application/associations.py
+++ application/associations.py
@@ -1,82 +1,100 @@
 import os
-import gnomevfs
+import gio
 
 from urllib import quote
-from ConfigParser import ConfigParser
+from collections import namedtuple
+
+
+ApplicationInfo = namedtuple(
+				'ApplicationInfo',
+				[
+					'id',
+					'name',
+					'description',
+					'executable',
+					'command_line',
+					'icon'
+				])
 
 
 class AssociationManager:
 	"""Class that provides 'Open With' menu"""
-
-	executable_types = (
-					'application/x-executable',
-					'application/x-shellscript',
-				)
-
-	def __init__(self):
-		self._config_section = 'Desktop Entry'
-		self._application_config_path = '/usr/share/applications'
-		self._user_path = os.path.expanduser('~/.local/share/applications')
+	
+	def __get_icon(self, icon_object):
+		"""Get icon string from GIO icon object"""
+		result = None
 		
+		if hasattr(icon_object, 'get_names'):
+			result = icon_object.get_names()[0]
+			
+		elif hasattr(icon_object, 'get_file'):
+			result = icon_object.get_file().get_path()
+			
+		return result
+
 	def get_mime_type(self, path):
 		"""Get mime type for specified path"""
-		return gnomevfs.get_mime_type(path)
+		return gio.content_type_guess(path)
 	
 	def get_mime_description(self, mime_type):
 		"""Get description from mime type"""
-		return gnomevfs.mime_get_description(mime_type)
+		return gio.content_type_get_description(mime_type)
 
-	def get_all_configs(self):
+	def get_all(self):
 		"""Return list of all applications"""
-		system_list = os.listdir(self._application_config_path)
-		system_list = map(lambda item: os.path.join(self._application_config_path, item), system_list)
-		
-		user_list = os.listdir(self._user_path)
-		user_list = map(lambda item: os.path.join(self._user_path, item), user_list)
-		
 		result = []
-		result.extend(system_list)
-		result.extend(user_list)
 		
-		result = filter(lambda item: os.path.splitext(item)[1] == '.desktop', result)
-		
+		for app_info in gio.app_info_get_all():
+			application = ApplicationInfo(
+									id = app_info.get_id(),
+									name = app_info.get_name(),
+									description = app_info.get_description(),
+									executable = app_info.get_executable(),
+									command_line = app_info.get_commandline(),
+									icon = self.__get_icon(app_info.get_icon())
+								)
+			
+			result.append(application)
+			
 		return result
 
 	def get_application_list_for_type(self, mime_type):
 		"""Get list of associated programs for specified type"""
-		return gnomevfs.mime_get_all_applications(mime_type)
+		result = []
+		
+		for app_info in gio.app_info_get_all_for_type(mime_type):
+			application = ApplicationInfo(
+									id = app_info.get_id(),
+									name = app_info.get_name(),
+									description = app_info.get_description(),
+									executable = app_info.get_executable(),
+									command_line = app_info.get_commandline(),
+									icon = self.__get_icon(app_info.get_icon())
+								)
+			
+			result.append(application)
+			
+		return result
 
 	def get_default_application_for_type(self, mime_type):
 		"""Get default application for specified type"""
-		return gnomevfs.mime_get_default_application(mime_type)
+		app_info = gio.app_info_get_default_for_type(mime_type, must_support_uris=False)
+		application = ApplicationInfo(
+								id = app_info.get_id(),
+								name = app_info.get_name(),
+								description = app_info.get_description(),
+								executable = app_info.get_executable(),
+								command_line = app_info.get_commandline(),
+								icon = self.__get_icon(app_info.get_icon())
+							)
+					
+		return application
 
-	def get_association_config(self, file_name):
-		"""Return dictionary containing all the options"""
-		result = None
-		config = ConfigParser()
-
-		if os.path.exists(os.path.join(self._user_path, file_name)):
-			config.read(os.path.join(self._user_path, file_name))
-
-		elif os.path.exists(os.path.join(self._application_config_path, file_name)):
-			config.read(os.path.join(self._application_config_path, file_name))
-
-		if config.has_section(self._config_section):
-			result = dict(config.items(self._config_section))
-
-		return result
-
-	def open_file(self, selection, config_file=None, exec_command=None):
+	def open_file(self, selection, application_info=None, exec_command=None):
 		"""Open filename using config file or specified execute command"""
-		if config_file is not None:
+		if application_info is not None:
 			# get command from config file
-			config = self.get_association_config(config_file)
-			
-			if config is None \
-			and not config.has_option(self._config_section, 'exec'): 
-				return
-	
-			command = config['exec']
+			command = application_info.command_line
 			
 		elif exec_command is not None:
 			# use specified command
@@ -130,15 +148,13 @@ class AssociationManager:
 	def execute_file(self, path):
 		"""Execute specified item"""
 		mime_type = self.get_mime_type(path)
-		is_executable = gnomevfs.is_executable_command_string(path)
+		is_executable = gio.content_type_can_be_executable(mime_type)
 
-		if mime_type in self.executable_types and is_executable:
+		if is_executable:
 			# file is executable type and has executable bit set
 			os.system('{0} &'.format(path))
 
 		else:
 			# file does not have executable bit set, open with default application
 			default_program = self.get_default_application_for_type(mime_type)
-			config_file = default_program[0]
-
-			self.open_file((path,), config_file)
+			self.open_file((path,), default_program)

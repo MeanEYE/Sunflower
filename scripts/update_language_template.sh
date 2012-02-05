be1e9dd32afd944ab602fd24c3c916072d684ac9
MeanEYE.rcf <meaneye.rcf@gmail.com>
2012-02-05 17:49:29 +0100

    Scripts: Updated script used for generating language template.

--- scripts/update_language_template.sh
+++ scripts/update_language_template.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd ..
+working_directory=`hg root`
+
+cd $working_directory
 find . -iname "*.py" | xargs xgettext --language=Python --package-name=Sunflower --package-version=0.1 --output translations/sunflower.pot

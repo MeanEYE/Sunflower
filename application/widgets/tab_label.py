7fefb4747b29a86e424829e39d6722f61ba6c9e1
Damián Nohales <damiannohales@gmail.com>
2012-02-22 18:21:26 -0300

    Now tabs can be closed with middle click.

--- application/widgets/tab_label.py
+++ application/widgets/tab_label.py
@@ -1,16 +1,23 @@
 import gtk
 import pango
 
-class TabLabel(gtk.HBox):
+class TabLabel(gtk.EventBox):
 	"""Tab label wrapper class"""
 
 	def __init__(self, application, parent):
-		gtk.HBox.__init__(self, False, 0)
+		gtk.EventBox.__init__(self)
 
 		self._application = application
 		self._parent = parent
+		
+		# initialize tab events
+		self.add_events(gtk.gdk.BUTTON_RELEASE_MASK)
+		self.connect('button-release-event', self._button_release_event)
 
 		# create interface
+		self._hbox = gtk.HBox(False, 0)
+		self.add(self._hbox)
+		
 		self._label = gtk.Label()
 		self._label.set_max_width_chars(20)
 		self._label.set_ellipsize(pango.ELLIPSIZE_END)
@@ -34,19 +41,30 @@ class TabLabel(gtk.HBox):
 		self._button.set_size_request(image_width + 2, image_height + 2)
 
 		# pack interface
-		self.pack_start(self._label, True, True, 0)
-		self.pack_start(self._button, False, False, 0)
+		self._hbox.pack_start(self._label, True, True, 0)
+		self._hbox.pack_start(self._button, False, False, 0)
 
 		# show controls
 		if self._application.options.getboolean('main', 'tab_close_button'):
 			self._button.show()
-			self.set_spacing(3)
-
+			self._hbox.set_spacing(3)
+		
 		self.show_all()
 
 	def _close_tab(self, widget, data=None):
 		"""Handle clicking on close button"""
 		self._application.close_tab(self._parent._notebook, self._parent)
+		
+	def _button_release_event(self, widget, event, data = None):
+		"""
+		Handle clicking on the tab itself, when middle button is pressed
+		the tab is closed.
+		"""
+		if event.button == 2:
+			self._close_tab(self._button)
+			return True
+		else:
+			return False
 
 	def set_text(self, text):
 		"""Set label text"""
@@ -56,8 +74,8 @@ class TabLabel(gtk.HBox):
 		"""Apply global settings to tab label"""
 		if self._application.options.getboolean('main', 'tab_close_button'):
 			self._button.show()
-			self.set_spacing(3)
+			self._hbox.set_spacing(3)
 
 		else:
 			self._button.hide()
-			self.set_spacing(0)
+			self._hbox.set_spacing(0)

31c44c37ff46d76e3500f54ef2c740c9f982c32c
MeanEYE.rcf <meaneye.rcf@gmail.com>
2012-04-15 19:23:10 +0200

    Plugins: File list: Gio wrapper: Implemented all methods.

--- application/plugins/file_list/gio_wrapper.py
+++ application/plugins/file_list/gio_wrapper.py
@@ -1,37 +1,63 @@
 import gio
 
+from plugin_base.provider import Mode
+
 
 class File:
 	"""This is a wrapper class that provides file-like object but
 	uses gio.File for actual operations."""
 
-	def __init__(self):
-		pass
+	def __init__(self, path, mode):
+		if mode == Mode.READ:
+			self._resource = gio.File(path).read()
+
+		elif mode == Mode.WRITE:
+			self._resource = gio.File(path).create()
+
+		elif mode == Mode.APPEND:
+			self._resource = gio.File(path).append_to()
 
 	def close(self):
 		"""Close file"""
-		pass
+		self._resource.close()
+
+	def closed(self):
+		"""If file is closed"""
+		self._resource.is_closed()
 
 	def flush(self):
 		"""Flush internal buffer"""
-		pass
+		if hasattr(self._resource, 'flush'):
+			self._resource.flush()
 
-	def read(self, size):
+	def read(self, size=-1):
 		"""Read at most _size_ bytes from the file"""
-		pass
+		result = self._resource.read(size)
 
-	def seek(offset, whence=None):
+		if result is True:
+			result = ""
+
+		return result
+
+	def seek(self, offset, whence=0):
 		"""Set the file's current position"""
-		pass
+		relative = (1, 0, 2)[whence]
+
+		if self._resource.can_seek():
+			self._resource.seek(offset, relative)
 
 	def tell(self):
 		"""Return file's current position"""
-		pass
+		return self._resource.tell()
 
 	def truncate(self, size=None):
 		"""Truncate the file's size"""
-		pass
+		if size is None:
+			size = self.tell()
+	
+		if self._resource.can_truncate():
+			self._resource.truncate(size)
 
 	def write(self, buff):
 		"""Write string to the file"""
-		pass
+		self._resource.write(buff)

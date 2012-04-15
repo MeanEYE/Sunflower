cdb098528ffcf2f54b9a2755f58441c8985f1eca
MeanEYE.rcf <meaneye.rcf@gmail.com>
2012-04-15 18:29:07 +0200

    Plugins: File list: Properly call rename_path.

--- application/plugins/file_list/plugin.py
+++ application/plugins/file_list/plugin.py
@@ -598,7 +598,7 @@ class FileList(ItemList):
 			if not self.get_provider().exists(result[1], relative_to=self.path):
 				try:
 					# rename selected item
-					self.get_provider().rename_path(selection, result[1])
+					self.get_provider().rename_path(selection, result[1], relative_to=self.path)
 
 					# mark item for selection after rename
 					self._item_to_focus = result[1]

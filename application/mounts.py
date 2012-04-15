8ac30da7e4d1155e1b7c0ec29d5ad95f22f6951e
MeanEYE.rcf <meaneye.rcf@gmail.com>
2012-04-15 16:29:35 +0200

    Mounts manager: Moved methods related to UI to window class.

--- application/mounts.py
+++ application/mounts.py
@@ -9,9 +9,7 @@ class MountsManager:
 
 	def __init__(self, application):
 		self._application = application
-		self._menu = None
-		self._menu_unmount = None
-		self._menu_item = None
+		self._mounts = {}
 
 		# create user interface
 		self.window = MountsManagerWindow(self)
@@ -33,12 +31,8 @@ class MountsManager:
 		for mount in self._volume_monitor.get_mounts():
 			self._add_mount(self._volume_monitor, mount)
 
-		# update menu item visibility based on mount count
-		self._menu_updated()
-
-	def _get_extension_for_selected_item(self):
-		"""Get extension for selected item in list, if possible"""
-		pass
+		# update menus
+		self.window._menu_updated()
 
 	def _add_mount(self, monitor, mount):
 		"""Catch volume-mounted signal and update mounts menu"""
@@ -53,24 +47,24 @@ class MountsManager:
 			self.window._volume_mounted(volume)
 
 		# add mount to the list
-		self.window.add_mount(mount_icon, mount.get_name(), mount_uri)
+		self.window._notify_mount_add(mount_icon, mount.get_name(), mount_uri)
 
 		# add bookmark menu item
-		self._add_item(
+		self.window._add_item(
 				mount.get_name(),
 				mount_uri,
 				mount_icon
 			)
 
 		# add unmount menu item
-		self._add_unmount_item(
+		self.window._add_unmount_item(
 				mount.get_name(),
 				mount_uri,
 				mount_icon
 			)
 
-		# notify system about menu update
-		self._menu_updated()
+		# add mount object to local cache list
+		self._mounts[mount_uri] = mount
 
 	def _remove_mount(self, monitor, mount):
 		"""Remove volume menu item from the mounts menu"""
@@ -82,13 +76,14 @@ class MountsManager:
 			self.window._volume_unmounted(volume)
 
 		# remove mount from list
-		self.window.remove_mount(mount_uri)
+		self.window._notify_mount_remove(mount_uri)
 
 		# remove item from menus
-		self._remove_item(mount_uri)
+		self.window._remove_item(mount_uri)
 
-		# notify system about menu update
-		self._menu_updated()
+		# remove mount object from cache list
+		if self._mounts.has_key(mount_uri):
+			self._mounts.pop(mount_uri)
 
 	def _add_volume(self, monitor, volume):
 		"""Event called when new volume is connected"""
@@ -97,50 +92,6 @@ class MountsManager:
 	def _remove_volume(self, monitor, volume):
 		"""Event called when volume is removed/unmounted"""
 		self.window._remove_volume(volume)
-
-	def _menu_updated(self):
-		"""Method called whenever menu is updated"""
-		has_mounts = len(self._menu.get_children()) > 1
-		self._menu_item_no_mounts.set_visible(not has_mounts)
-		self._menu_item_no_mounts2.set_visible(not has_mounts)
-
-	def _add_item(self, text, uri, icon):
-		"""Add new menu item to the list"""
-		image = gtk.Image()
-		image.set_from_icon_name(icon, gtk.ICON_SIZE_MENU)
-
-		menu_item = gtk.ImageMenuItem()
-		menu_item.set_label(text)
-		menu_item.set_image(image)
-		menu_item.set_always_show_image(True)
-		menu_item.set_data('path', uri)
-		menu_item.connect('activate', self._application._handle_bookmarks_click)
-		menu_item.show()
-
-		self._menu.append(menu_item)
-
-	def _add_unmount_item(self, text, uri, icon):
-		"""Add new menu item used for unmounting"""
-		image = gtk.Image()
-		image.set_from_icon_name(icon, gtk.ICON_SIZE_MENU)
-
-		menu_item = gtk.ImageMenuItem()
-		menu_item.set_label(text)
-		menu_item.set_image(image)
-		menu_item.set_always_show_image(True)
-		menu_item.set_data('uri', uri)
-		menu_item.connect('activate', self._unmount_item)
-		menu_item.show()
-
-		self._menu_unmount.append(menu_item)
-
-	def _remove_item(self, mount_point):
-		"""Remove item based on device name"""
-		for item in self._menu.get_children():
-			if item.get_data('uri') == mount_point: self._menu.remove(item)
-
-		for item in self._menu_unmount.get_children():
-			if item.get_data('uri') == mount_point: self._menu_unmount.remove(item)
 
 	def _unmount_item(self, widget, data=None):
 		"""Event called by the unmount menu item or unmount button from manager"""
@@ -158,6 +109,11 @@ class MountsManager:
 					self._unmount(mount)
 					break
 
+	def _unmount_by_uri(self, uri):
+		"""Perform unmount by URI"""
+		if self._mounts.has_key(uri):
+			self._unmount(self._mounts[uri])
+		
 	def _unmount(self, mount):
 		"""Perform unmounting"""
 		if mount.can_unmount():
@@ -180,31 +136,10 @@ class MountsManager:
 		"""Callback for unmount events"""
 		# try to finish async unmount
 		mount.unmount_finish(result)
-		mount_uri = mount.get_root().get_uri()
 
 	def _attach_menus(self):
 		"""Attach mounts to menus"""
-		menu_manager = self._application.menu_manager
-
-		# get mounts menu item
-		self._menu_item = self._application._menu_item_mounts
-
-		# create mounts menu
-		self._menu = gtk.Menu()
-		self._menu_item.set_submenu(self._menu)
-
-		self._menu_unmount = menu_manager.get_item_by_name('unmount_menu').get_submenu()
-
-		# create item for usage when there are no mounts
-		self._menu_item_no_mounts = gtk.MenuItem(label=_('Mount list is empty'))
-		self._menu_item_no_mounts.set_sensitive(False)
-		self._menu_item_no_mounts.set_property('no-show-all', True)
-		self._menu.append(self._menu_item_no_mounts)
-
-		self._menu_item_no_mounts2 = menu_manager.get_item_by_name('mount_list_empty')
-		self._menu_item_no_mounts2.set_property('no-show-all', True)
-
-		self._populate_list()
+		self.window._attach_menus()
 
 	def show(self, widget=None, data=None):
 		"""Show mounts manager window"""

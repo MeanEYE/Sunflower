5122bc7f4d412628bd5daa080a56bb3fcc060209
MeanEYE.rcf <meaneye.rcf@gmail.com>
2012-06-14 00:52:54 +0200

    Created keyring manager.

---
+++ application/keyring.py
@@ -0,0 +1,53 @@
+import gtk
+import threading
+
+try:
+	import gnomekeyring as keyring
+
+except:
+	keyring = None
+
+
+class EntryType:
+	NO_TYPE = 0
+	GENERIC = 1
+	NETWORK = 2
+	NOTE = 3
+
+
+class KeyringManager:
+	"""Keyring manager is used to securely store passwords. It also
+	manages keyring availability and provides automatic locking.
+
+	"""
+
+	KEYRING_NAME = 'sunflower'
+	TIMEOUT = 10
+
+	def __init__(self, application):
+		self._application = application
+		self._thread = None
+
+	def __lock_keyring(self):
+		"""Method called after specified amount of time has passed"""
+		pass
+
+	def __unlock_keyring(self):
+		"""Unlock keyring and schedule automatic lock"""
+		pass
+
+	def is_available(self):
+		"""Return true if we are able to use Gnome keyring"""
+		return keyring is not None
+
+	def get_entries(self):
+		"""Return list of tuples containing entry names and description"""
+		pass
+
+	def get_password(self, entry):
+		"""Return password for specified entry"""
+		pass
+
+	def store_password(self, entry, description, password, p_type=None):
+		"""Create new entry in keyring with specified data"""
+		pass

8b4b6661809d17d307af408ef9ad7d8fbb884d8f
MeanEYE.rcf <meaneye.rcf@gmail.com>
2012-06-26 20:12:27 +0200

    Configuration provider is functional now.

--- application/config.py
+++ application/config.py
@@ -1,9 +1,32 @@
+import os
 import json
 
 
-class Config:
+class Container:
+	"""Generic configuration container"""
+
+	def __init__(self, data=None):
+		self._values = {}
+
+		if data is not None:
+			self._values = data
+
+	def _get_data(self):
+		"""Get data for storage"""
+		return self._values.copy()
+
+	def set(self, name, value):
+		"""Set configuration value"""
+		self._values[name] = value
+
+	def get(self, name):
+		"""Get configuration value"""
+		return self._values[name] if name in self._values else None
+
+
+class Config(Container):
 	"""This class provides easy way to create and edit configuration files
-	located in Sunflower's configuration directory. 
+	located in project's configuration directory. 
 	
 	It is recomended that this class is used for all purposes of storing 
 	data used by program itself and	plugins!
@@ -14,32 +37,76 @@ class Config:
 				'skipkeys': True,
 				'check_circular': True,
 				'sort_keys': True,
-				'indent': 1
+				'indent': 4
 			}
 
-	def __init__(self, name):
+	def __init__(self, name, application):
+		Container.__init__(self)
+
 		self._name = name
 		self._sections = {}
+		self._application = application
 
-		self._encoder = json.JSONEncoder(*self.encoder_options)
+		self._encoder = json.JSONEncoder(**self.encoder_options)
 		self._decoder = json.JSONDecoder()
+
+		# try to load config file
+		self.load()
 
 	def save(self):
 		"""Save options to configuration file"""
-		pass
+		data = self._get_data()
+		file_name = os.path.join(
+						self._application.config_path,
+						'{0}.json'.format(self._name)
+					)
+
+		# merge sections with main values
+		for name, section in self._sections.items():
+			data[name] = section._get_data()
+
+		# save output to file
+		with open(file_name, 'w') as raw_file:
+			raw_file.write(self._encoder.encode(data))
 
 	def load(self):
 		"""Load options from configuration file"""
-		pass
+		file_name = os.path.join(
+						self._application.config_path,
+						'{0}.json'.format(self._name)
+					)
+
+		if not os.path.exists(file_name):
+			return
+
+		try:
+			# try loading config file
+			data = self._decoder.decode(open(file_name).read())
+
+		except ValueError:
+			# we error occurs, we'll just ignore it
+			# empty config is not that scary
+			pass
+	
+		else:
+			# finish the loading
+			for name, values in data.items():
+				if type(values) is dict:
+					# section
+					self._sections[name] = Container(values)
+
+				else:
+					# normal value
+					self._values[name] = values
 
 	def add_section(self, name, section):
 		"""Add new section to configutation"""
-		self._values[name] = section
+		self._sections[name] = section
 
 	def create_section(self, name):
 		"""Create and return new section object"""
-		section = Section()
-		self._values[name] = section
+		section = Container()
+		self._sections[name] = section
 
 		return section
 
@@ -57,23 +124,3 @@ class Config:
 		return name in self._sections
 
 
-class Container:
-	"""Generic configuration container"""
-
-	def __init__(self, data=None):
-		self._values = {}
-
-		if data is not None:
-			self._values = data
-
-	def _get_data(self):
-		"""Get data for storage"""
-		return self._values
-
-	def set(self, name, value):
-		"""Set configuration value"""
-		self._values[name] = value
-
-	def get(self, name):
-		"""Get configuration value"""
-		return self._values[name] if name in self._values else None

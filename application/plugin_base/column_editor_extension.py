b426229627e7c88f2216d48382646a7adc08cadd
MeanEYE.rcf <meaneye.rcf@gmail.com>
2012-07-24 20:58:32 +0200

    Plugin base: Added column editor extension base class.

---
+++ application/plugin_base/column_editor_extension.py
@@ -0,0 +1,38 @@
+class ColumnEditorExtension:
+	"""Class used for extending column editor in preferences window"""
+
+	def __init__(self, parent, config):
+		self._parent = parent
+		self._parent_name = parent._name
+		self._config = config
+
+	def _save_settings(self):
+		"""Save values to config"""
+		pass
+
+	def _load_settings(self):
+		"""Load values from config"""
+		pass
+
+	def get_name(self):
+		"""Get plugin's name"""
+		return self._parent_name
+
+	def get_columns(self, only_visible=False):
+		"""Get column names"""
+
+	def get_size(self, column):
+		"""Get column size"""
+		pass
+
+	def get_visible(self, column):
+		"""Get column visibility"""
+		pass
+
+	def set_size(self, column, size):
+		"""Set column size"""
+		pass
+
+	def set_visible(self, column, visible):
+		"""Set column visibility"""
+		pass

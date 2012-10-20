f591d2394b65653a840ce99d1964d9195eb8aa5e
MeanEYE.rcf <meaneye.rcf@gmail.com>
2012-10-20 15:27:47 +0200

    Plugins: File list: Gio extension: Added empty FTP extension class.

--- application/plugins/file_list/gio_extension.py
+++ application/plugins/file_list/gio_extension.py
@@ -16,6 +16,15 @@ class SambaColumn:
 	USERNAME = 5
 	REQUIRES_LOGIN = 6
 	URI = 7
+
+
+class FTPColumn:
+	NAME = 0
+	SERVER = 1
+	DIRECTORY = 2
+	USERNAME = 3
+	REQUIRES_LOGIN = 4
+	URI = 7
 	
 
 class SambaExtension(MountManagerExtension):
@@ -39,8 +48,8 @@ class SambaExtension(MountManagerExtension):
 		cell_name = gtk.CellRendererText()
 		cell_uri = gtk.CellRendererText()
 
-		col_name = gtk.TreeViewSambaColumn(_('Name'), cell_name, text=SambaColumn.NAME)
-		col_uri = gtk.TreeViewSambaColumn(_('URI'), cell_uri, text=SambaColumn.URI)
+		col_name = gtk.TreeViewColumn(_('Name'), cell_name, text=SambaColumn.NAME)
+		col_uri = gtk.TreeViewColumn(_('URI'), cell_uri, text=SambaColumn.URI)
 
 		col_name.set_expand(True)
 
@@ -461,3 +470,141 @@ class SambaExtension(MountManagerExtension):
 		return 'samba', 'Samba'
 
 
+class FTPExtension(MountManagerExtension):
+	"""Mount manager extension that provides editing and mounting
+	of FTP (and SFTP) shares through GIO backend.
+
+	"""
+	features = set([ExtensionFeatures.SYSTEM_WIDE,])
+
+	def __init__(self, parent, window):
+		MountManagerExtension.__init__(self, parent, window)
+
+		# create user interface
+		list_container = gtk.ScrolledWindow()
+		list_container.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+		list_container.set_shadow_type(gtk.SHADOW_IN)
+
+		self._store = gtk.ListStore(str, str, str, str, bool, str) 
+		self._list = gtk.TreeView(model=self._store)
+
+		cell_name = gtk.CellRendererText()
+		cell_uri = gtk.CellRendererText()
+
+		col_name = gtk.TreeViewColumn(_('Name'), cell_name, text=SambaColumn.NAME)
+		col_uri = gtk.TreeViewColumn(_('URI'), cell_uri, text=SambaColumn.URI)
+
+		col_name.set_expand(True)
+
+		self._list.append_column(col_name)
+		self._list.append_column(col_uri)
+
+		# create controls
+		image_add = gtk.Image()
+		image_add.set_from_stock(gtk.STOCK_ADD, gtk.ICON_SIZE_BUTTON)
+
+		button_add = gtk.Button()
+		button_add.set_image(image_add)
+		button_add.connect('clicked', self._add_mount)
+
+		image_edit = gtk.Image()
+		image_edit.set_from_stock(gtk.STOCK_EDIT, gtk.ICON_SIZE_BUTTON)
+
+		button_edit = gtk.Button()
+		button_edit.set_image(image_edit)
+		button_edit.connect('clicked', self._edit_mount)
+
+		image_delete = gtk.Image()
+		image_delete.set_from_stock(gtk.STOCK_DELETE, gtk.ICON_SIZE_BUTTON)
+
+		button_delete = gtk.Button()
+		button_delete.set_image(image_delete)
+		button_delete.connect('clicked', self._delete_mount)
+
+		button_mount = gtk.Button(_('Mount'))
+		button_mount.connect('clicked', self._mount_selected)
+
+		button_unmount = gtk.Button(_('Unmount'))
+		button_unmount.connect('clicked', self._unmount_selected)
+
+		# use spinner if possible to denote busy operation
+		if hasattr(gtk, 'Spinner'):
+			self._spinner = gtk.Spinner()
+			self._spinner.set_size_request(20, 20)
+			self._spinner.set_property('no-show-all', True)
+
+		else:
+			self._spinner = None
+
+		# pack user interface
+		list_container.add(self._list)
+		
+		self._container.pack_start(list_container, True, True, 0)
+
+		self._controls.pack_start(button_add, False, False, 0)
+		self._controls.pack_start(button_edit, False, False, 0)
+		self._controls.pack_start(button_delete, False, False, 0)
+
+		if self._spinner is not None:
+			self._controls.pack_start(self._spinner, False, False, 0)
+		self._controls.pack_end(button_unmount, False, False, 0)
+		self._controls.pack_end(button_mount, False, False, 0)
+
+		# load entries from config file
+		self.__populate_list()
+
+	def __populate_list(self):
+		"""Populate list with stored mounts"""
+		pass
+
+	def __form_uri(self, server, username, directory):
+		"""Form URI string from specified parameters"""
+		pass
+
+	def __store_mount(self, params, uri):
+		"""Store mount to the list"""
+		pass
+
+	def __save_list(self):
+		"""Save list to config file"""
+		pass
+
+	def __mount(self, uri, domain, username, password=None):
+		"""Mount selected mount"""
+		pass
+
+	def __unmount(self, uri):
+		"""Unmount selected mount"""
+		pass
+
+	def __mount_callback(self, path, result):
+		"""Finish mounting"""
+		pass
+
+	def __unmount_callback(self, mount, result):
+		"""Finish unmounting"""
+		pass
+
+	def _add_mount(self, widget, data=None):
+		"""Show dialog for adding new FTP mount"""
+		pass
+
+	def _delete_mount(self, widget, data=None):
+		"""Remove selected mount"""
+		pass
+
+	def _edit_mount(self, widget, data=None):
+		"""Edit selected mount"""
+		pass
+
+	def _mount_selected(self, widget, data=None):
+		"""Mount selected"""
+		pass
+
+	def _unmount_selected(self, widget, data=None):
+		"""Unmount selected"""
+		pass
+
+	def get_information(self):
+		"""Get extension information"""
+		return 'folder-remote-ftp', 'FTP'

7f05f5ca4e98a7ecddc206f8a6620628c9f2505c
MeanEYE.rcf <meaneye.rcf@gmail.com>
2012-10-24 23:56:53 +0200

    Preferences: Item list: Properly save option used to reserve file size on copy/move.

--- application/gui/preferences/item_list.py
+++ application/gui/preferences/item_list.py
@@ -384,7 +384,7 @@ class ItemListOptions(SettingsPage):
 		section.set('case_sensitive_sort', self._checkbox_case_sensitive.get_active())
 		section.set('right_click_select', self._checkbox_right_click.get_active())
 		options.section('operations').set('trash_files', self._checkbox_trash_files.get_active())
-		section.set('reserve_size', self._checkbox_reserve_size.get_active())
+		options.section('operations').set('reserve_size', self._checkbox_reserve_size.get_active())
 		section.set('headers_visible', self._checkbox_show_headers.get_active())
 		options.set('media_preview', self._checkbox_media_preview.get_active())
 		section.set('mode_format', self._combobox_mode_format.get_active())

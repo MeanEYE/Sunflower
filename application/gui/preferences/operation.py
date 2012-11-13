f7a5fe043c01e06d0010b8aa871c473705520928
MeanEYE.rcf <meaneye.rcf@gmail.com>
2012-11-13 23:06:40 +0100

    Preferences: Operation: Added checkbox to allow disabling confirmation when deleting items.

--- application/gui/preferences/operation.py
+++ application/gui/preferences/operation.py
@@ -18,16 +18,22 @@ class OperationOptions(SettingsPage):
 		vbox_mounts = gtk.VBox(False, 0)
 		vbox_mounts.set_border_width(5)
 
+		frame_confirmations = gtk.Frame(_('Confirmation'))
+		vbox_confirmations = gtk.VBox(False, 0)
+		vbox_confirmations.set_border_width(5)
+
 		# create components
 		self._checkbox_trash_files = gtk.CheckButton(_('Delete items to trash can'))
 		self._checkbox_reserve_size = gtk.CheckButton(_('Reserve free space on copy/move'))
 		self._checkbox_automount_on_start = gtk.CheckButton(_('Automount drives on start up'))
 		self._checkbox_automount_on_insert = gtk.CheckButton(_('Automount removable drives when inserted'))
+		self._checkbox_confirm_delete = gtk.CheckButton(_('Show confirmation dialog before deleting items'))
 
 		self._checkbox_trash_files.connect('toggled', self._parent.enable_save)
 		self._checkbox_reserve_size.connect('toggled', self._parent.enable_save)
 		self._checkbox_automount_on_start.connect('toggled', self._parent.enable_save)
 		self._checkbox_automount_on_insert.connect('toggled', self._parent.enable_save)
+		self._checkbox_confirm_delete.connect('toggled', self._parent.enable_save)
 
 		# pack user interface
 		vbox_general.pack_start(self._checkbox_trash_files, False, False, 0)
@@ -36,30 +42,38 @@ class OperationOptions(SettingsPage):
 		vbox_mounts.pack_start(self._checkbox_automount_on_start, False, False, 0)
 		vbox_mounts.pack_start(self._checkbox_automount_on_insert, False, False, 0)
 
+		vbox_confirmations.pack_start(self._checkbox_confirm_delete, False, False, 0)
+
 		frame_general.add(vbox_general)
 		frame_mounts.add(vbox_mounts)
+		frame_confirmations.add(vbox_confirmations)
 
 		self.pack_start(frame_general, False, False, 0)
 		self.pack_start(frame_mounts, False, False, 0)
+		self.pack_start(frame_confirmations, False, False, 0)
 
 	def _load_options(self):
 		"""Load item list options"""
 		options = self._application.options
-		section = options.section('operations')
+		operations = options.section('operations')
+		confirmations = options.section('confirmations')
 
 		# load options
-		self._checkbox_trash_files.set_active(section.get('trash_files'))
-		self._checkbox_reserve_size.set_active(section.get('reserve_size'))
-		self._checkbox_automount_on_start.set_active(section.get('automount_start'))
-		self._checkbox_automount_on_insert.set_active(section.get('automount_insert'))
+		self._checkbox_trash_files.set_active(operations.get('trash_files'))
+		self._checkbox_reserve_size.set_active(operations.get('reserve_size'))
+		self._checkbox_automount_on_start.set_active(operations.get('automount_start'))
+		self._checkbox_automount_on_insert.set_active(operations.get('automount_insert'))
+		self._checkbox_confirm_delete.set_active(confirmations.get('delete_items'))
 
 	def _save_options(self):
 		"""Save item list options"""
 		options = self._application.options
-		section = options.section('operations')
+		operations = options.section('operations')
+		confirmations = options.section('confirmations')
 
 		# save settings
-		section.set('trash_files', self._checkbox_trash_files.get_active())
-		section.set('reserve_size', self._checkbox_reserve_size.get_active())
-		section.set('automount_start', self._checkbox_automount_on_start.get_active())
-		section.set('automount_insert', self._checkbox_automount_on_insert.get_active())
+		operations.set('trash_files', self._checkbox_trash_files.get_active())
+		operations.set('reserve_size', self._checkbox_reserve_size.get_active())
+		operations.set('automount_start', self._checkbox_automount_on_start.get_active())
+		operations.set('automount_insert', self._checkbox_automount_on_insert.get_active())
+		confirmations.set('delete_items', self._checkbox_confirm_delete.get_active())

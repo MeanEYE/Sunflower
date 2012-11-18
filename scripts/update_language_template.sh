a232350908abf557863e5a195b6118f3f8a84ef2
MeanEYE.rcf <meaneye.rcf@gmail.com>
2012-11-18 18:33:42 +0100

    Scripts: Update language template: Simplified it a bit and added output messages.

--- scripts/update_language_template.sh
+++ scripts/update_language_template.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-working_directory=`hg root`
 
-cd $working_directory
+echo -n "Updating language template..."
+cd `hg root`
 find . -iname "*.py" | xargs xgettext --language=Python --package-name=Sunflower --package-version=0.1 --output translations/sunflower.pot
+echo "done!"

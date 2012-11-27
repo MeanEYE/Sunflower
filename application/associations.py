1c98c1f78fde36a55e81e4377ec49b47a21556a2
MeanEYE.rcf <meaneye.rcf@gmail.com>
2012-11-27 01:03:06 +0100

    Associations manager: Added ability to open files iside of terminal tabs.

--- application/associations.py
+++ application/associations.py
@@ -6,6 +6,7 @@ import subprocess
 
 from common import is_x_app
 from collections import namedtuple
+from parameters import Parameters
 from plugin_base.provider import Mode
 from plugin_base.terminal import TerminalType
 from gui.input_dialog import ApplicationSelectDialog
@@ -197,10 +198,15 @@ class AssociationManager:
 			os.system('{0} &'.format(exec_string))
 
 		else:
-			self._application.execute_command_in_terminal_tab(
-									command=exec_string,
-									close_with_child=True
-								)
+			active_object = self._application.get_active_object()
+
+			options = Parameters()
+			options.set('close_with_child', True)
+			options.set('shell_command', split_command[0])
+			options.set('arguments', split_command)
+			options.set('path', os.path.dirname(selection[0]))
+
+			self._application.create_terminal_tab(active_object._notebook, options)
 
 	def edit_file(self, selection):
 		"""Edit selected filename"""
@@ -213,12 +219,17 @@ class AssociationManager:
 		split_command = shlex.split(exec_string)
 		test_command = split_command[0] if len(split_command) > 1 else exec_string
 
-		if section.get('terminal_command') \
+		if (section.get('terminal_command') and section.get('type') == 1) \
 		or not is_x_app(test_command):
-			self._application.execute_command_in_terminal_tab(
-									command=exec_string,
-									close_with_child=True
-								)
+			active_object = self._application.get_active_object()
+
+			options = Parameters()
+			options.set('close_with_child', True)
+			options.set('shell_command', split_command[0])
+			options.set('arguments', split_command)
+			options.set('path', os.path.dirname(selection[0]))
+
+			tab = self._application.create_terminal_tab(active_object._notebook, options)
 
 		else:
 			os.system('{0} &'.format(exec_string))
@@ -251,13 +262,13 @@ class AssociationManager:
 				# command is console based, create terminal tab and fork it
 				if terminal_type != TerminalType.EXTERNAL:
 					active_object = self._application.get_active_object()
-					tab = self._application.create_terminal_tab(active_object._notebook)
 
-					tab._close_on_child_exit = False
-					tab._terminal.fork_command(
-									command=path,
-									directory=os.path.dirname(path)
-								)
+					options = Parameters()
+					options.set('close_with_child', False)
+					options.set('shell_command', path)
+					options.set('path', os.path.dirname(path))
+
+					tab = self._application.create_terminal_tab(active_object._notebook, options)
 
 		else:
 			# file type is not executable, try to open with default associated application

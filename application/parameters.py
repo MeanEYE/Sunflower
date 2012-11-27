9da87b360930af752b8185112e77750fa2edd5f9
MeanEYE.rcf <meaneye.rcf@gmail.com>
2012-11-27 01:07:16 +0100

    Parameters container: Created methods to allow duplication. Added constructor params as well.

--- application/parameters.py
+++ application/parameters.py
@@ -1,8 +1,8 @@
 class Parameters:
 	"""Simple class used as storage container for various parameters"""
 
-	def __init__(self):
-		self._parameters = {}
+	def __init__(self, params=None):
+		self._parameters = {} if params is None else params.copy()
 
 	def get(self, name, default=None):
 		"""Get parameter"""
@@ -11,3 +11,11 @@ class Parameters:
 	def set(self, name, value):
 		"""Set parameter"""
 		self._parameters[name] = value
+
+	def get_params(self):
+		"""Get copy of parameters"""
+		return self._parameters.copy()
+
+	def copy(self):
+		"""Return copy of parameters"""
+		return Parameters(self.get_params())

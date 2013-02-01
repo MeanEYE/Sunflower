b7714156afe3fd06c84bc7df38731db0201417a0
MeanEYE.rcf <meaneye.rcf@gmail.com>
2013-02-01 20:01:30 +0100

    Plugins: Owner column: Separated classes for owner and group columns.

--- application/plugins/owner_column/plugin.py
+++ application/plugins/owner_column/plugin.py
@@ -1,16 +1,17 @@
 import gtk
 
-from plugin_base.item_list import ItemList
+from plugins.file_list.plugin import Column, FileList
 from plugin_base.column_extension import ColumnExtension
 
 
 def register_plugin(application):
 	"""Register plugin class with application"""
-	application.register_column_extension(ItemList, OwnerColumn)
+	application.register_column_extension(FileList, OwnerColumn)
+	application.register_column_extension(FileList, GroupColumn)
 
 
-class OwnerColumn(ColumnExtension):
-	"""Adds support for displaying owner and group in item list"""
+class BaseColumn(ColumnExtension):
+	"""Base class for extending owner and group for item list"""
 
 	def __init__(self, parent, store):
 		ColumnExtension.__init__(self, parent, store)
@@ -18,26 +19,79 @@ class OwnerColumn(ColumnExtension):
 		# create column object
 		self._create_column()
 
+	def _create_column(self):
+		"""Create column"""
+		self._cell_renderer = gtk.CellRendererText()
+		self._cell_renderer.set_property('size-points', 8)
+
+		self._column = gtk.TreeViewColumn(self._get_column_title())
+		self._column.pack_start(self._cell_renderer, True)
+		self._column.set_data('name', self._get_column_name())
+
+	def _get_column_name(self):
+		"""Returns column name"""
+		return None
+
+	def _get_column_title(self):
+		"""Returns column title"""
+		return None
+
 	def __set_cell_data(self, column, cell, store, selected_iter, data=None):
 		"""Set column value"""
-		is_parent = store.get_value(selected_iter, 10)
+		pass
 
-		value = (store.get_value(selected_iter, 14), '')[is_parent]
+
+class OwnerColumn(BaseColumn):
+	"""Adds support for displaying owner in item list"""
+
+	def __set_cell_data(self, column, cell, store, selected_iter, data=None):
+		"""Set column value"""
+		is_parent = store.get_value(selected_iter, Column.IS_PARENT_DIR)
+
+		value = (store.get_value(selected_iter, Column.USER_ID), '')[is_parent]
 		cell.set_property('text', value)
 
 	def _create_column(self):
-		"""Create column"""
-		cell_renderer = gtk.CellRendererText()
-		cell_renderer.set_property('size-points', 8)
+		"""Configure column"""
+		BaseColumn._create_column(self)
+		self._column.set_cell_data_func(self._cell_renderer, self.__set_cell_data)
 
-		self._column = gtk.TreeViewColumn(_('Owner'))
-		self._column.pack_start(cell_renderer, True)
-		self._column.set_data('name', 'owner')
-		self._column.set_resizable(True)
-		self._column.set_sizing(gtk.TREE_VIEW_COLUMN_FIXED)
-		self._column.connect('clicked', self._parent._set_sort_function, 14)
-		self._column.set_cell_data_func(cell_renderer, self.__set_cell_data)
+	def _get_column_name(self):
+		"""Returns column name"""
+		return 'owner'
 
-	def get_column(self):
-		"""Get column object to be added to the list"""
-		return self._column
+	def _get_column_title(self):
+		"""Returns column title"""
+		return _('Owner')
+
+	def get_sort_column(self):
+		"""Return sort column"""
+		return Column.USER_ID
+
+
+class GroupColumn(BaseColumn):
+	"""Adds support for displaying group in item list"""
+
+	def __set_cell_data(self, column, cell, store, selected_iter, data=None):
+		"""Set column value"""
+		is_parent = store.get_value(selected_iter, Column.IS_PARENT_DIR)
+
+		value = (store.get_value(selected_iter, Column.GROUP_ID), '')[is_parent]
+		cell.set_property('text', value)
+
+	def _create_column(self):
+		"""Configure column"""
+		BaseColumn._create_column(self)
+		self._column.set_cell_data_func(self._cell_renderer, self.__set_cell_data)
+
+	def _get_column_name(self):
+		"""Returns column name"""
+		return 'group'
+
+	def _get_column_title(self):
+		"""Returns column title"""
+		return _('Group')
+
+	def get_sort_column(self):
+		"""Return sort column"""
+		return Column.GROUP_ID

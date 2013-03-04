2fedf632d98b723ecc1126226a61dbf1025f7e56
Wojciech Kluczka <wojtekkluczka@gmail.com>
2013-03-04 22:49:57 +0100

    Scripts: Removed scripts replaced by Makefile

--- scripts/update_language_template.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo -n "Updating language template..."
-cd `hg root`
-find . -iname "*.py" | xargs xgettext --language=Python --package-name=Sunflower --package-version=0.1 --output translations/sunflower.pot
-echo "done!"

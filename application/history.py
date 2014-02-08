cd109c7887156b0c968c5962320ab804ff860e30
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-02-08 18:47:26 +0200

    History manager: Added support for selecting row if two paths match.

--- application/history.py
+++ application/history.py
@@ -1,3 +1,4 @@
+import os
 
 
 class HistoryManager:
@@ -7,6 +8,19 @@ class HistoryManager:
 		self._list = storage_list
 		self._index = 0
 		self._parent = parent
+
+	def _change_to_index(self, index):
+		"""Change parent path to specified index"""
+		new_path = self._list[index]
+		current_path = self._parent._options.get('path')
+
+		# determine if row needs to be selected
+		selection = None
+		if current_path.startswith(new_path):
+			selection = os.path.basename(current_path)
+
+		# change path
+		self._parent.change_path(new_path, selection)
 
 	def record(self, path):
 		"""Record new path in history"""
@@ -18,16 +32,16 @@ class HistoryManager:
 
 	def back(self):
 		"""Go back in history one step"""
-		if self._index >= len(self._list):
+		if self._index >= len(self._list) - 1:
 			return
 
-		new_index = self._index + 1
-		self._parent.change_path(self._list[new_index])
+		# change path
+		self._change_to_index(self._index + 1)
 
 	def forward(self):
 		"""Go forward in history one step"""
 		if self._index < 1:
 			return
 
-		new_index = self._index - 1
-		self._parent.change_path(self._list[new_index])
+		# change path
+		self._change_to_index(self._index - 1)

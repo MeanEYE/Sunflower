f7020e8e6bd28ce4ed43581ad6313a04f40328b7
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-07-25 12:18:00 +0200

    DBus interface: Added assert commands to all methods going through DBus.

--- application/dbus_interface.py
+++ application/dbus_interface.py
@@ -15,41 +15,55 @@ class DBus_Client():
 		self.connect()
 
 	def connect(self):
+		"""Connect client to DBus service."""
 		try:
 			self._proxy = self._bus.get_object(self._bus_name, self._path)
+
 		except dbus.exceptions.DBusException:
 			self._proxy = None
 
 	def disconnect(self):
-		if self.is_connected():
+		"""Disconnect from DBus service."""
+		if self._proxy is not None:
 			self._proxy.close()
 
 	def is_connected(self):
-		return True if self._proxy else None
+		return True if self._proxy else False
 
 	def show_window(self):
 		"""Send request to show window through DBus."""
+		assert self._proxy is not None
 		self._proxy.get_dbus_method('show_window')()
 
 	def create_tab(self, path, position=None):
+		"""Create tab in running instance."""
+		assert self._proxy is not None
 		create_tab = self._proxy.get_dbus_method('create_tab')
 		create_tab(path, position)
 
 	def create_terminal(self, path, position=None):
+		"""Create terminal tab in running instance."""
+		assert self._proxy is not None
 		create_terminal = self._proxy.get_dbus_method('create_terminal')
 		create_terminal(path, position)
 
 	def one_instance(self):
+		"""Make create tabs, focus existing instance window and exit."""
 		arguments = self._application.arguments
+
 		if arguments is not None:
 			if arguments.left_tabs is not None:
-				map(self.create_tab, arguments.left_tabs, ['left']*len(arguments.left_tabs))
+				map(self.create_tab, arguments.left_tabs, ['left'] * len(arguments.left_tabs))
+
 			if arguments.right_tabs is not None:
-				map(self.create_tab, arguments.right_tabs, ['right']*(len(arguments.right_tabs)))
+				map(self.create_tab, arguments.right_tabs, ['right'] * len(arguments.right_tabs))
+
 			if self._application.arguments.left_terminals is not None:
-				map(self.create_terminal, arguments.left_terminals, ['left']*(len(arguments.left_terminals)))
+				map(self.create_terminal, arguments.left_terminals, ['left'] * len(arguments.left_terminals))
+
 			if self._application.arguments.right_terminals is not None:
-				map(self.create_terminal, arguments.right_terminals, ['right']*(len(arguments.right_terminals)))
+				map(self.create_terminal, arguments.right_terminals, ['right'] * len(arguments.right_terminals))
+
 		self.show_window()
 		sys.exit()
 

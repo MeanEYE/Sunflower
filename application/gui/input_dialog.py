5cd10f09288a8c502df558214ec37556d0e25259
Christian Mallwitz <c.mallwitz@gmail.com>
2016-03-21 21:34:29 +0000

    Fix handling of file deletion error/retry

--- application/gui/input_dialog.py
+++ application/gui/input_dialog.py
@@ -1492,19 +1492,6 @@ class OperationError:
 		return code
 
 
-class QuestionOperationError(OperationError):
-	"""Operation error with modified buttons labels."""
-
-	def __init__(self, application):
-		OperationError.__init__(self, application)
-
-		button_no = self._dialog.get_widget_for_response(Gtk.ResponseType.NO)
-		button_yes = self._dialog.get_widget_for_response(Gtk.ResponseType.YES)
-
-		button_no.set_label(_('No'))
-		button_yes.set_label(_('Yes'))
-
-
 class CreateToolbarWidgetDialog:
 	"""Create widget persisten dialog."""
 

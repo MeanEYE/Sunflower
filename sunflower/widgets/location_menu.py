447807379a30297f86173e8802c8dc7f3d56a5a1
Mladen Mijatov <meaneye.rcf@gmail.com>
2019-02-24 08:18:42 +0100

    Location menu: Restructured to remove tabs.
    From testing tabs seem to pose additional obstacle when operating the
    program only with keyboard as it's easier to switch to different pages
    using mouse than complicated shortcuts.
    
    Consolidated interface in a single page with properly done search
    matching function significantly improves navigating locations menu.

--- sunflower/widgets/location_menu.py
+++ sunflower/widgets/location_menu.py
@@ -14,7 +14,9 @@ class LocationMenu:
 
 	def __init__(self, application):
 		self._application = application
-		self._control = None
+		self._active_object = None
+		self._headers = {}
+		self._assigned = {}
 
 		# create popover interface
 		self._popover = Gtk.Popover.new()
@@ -27,11 +29,18 @@ class LocationMenu:
 
 		# create search field
 		self._search_field = Gtk.SearchEntry.new()
+		self._search_field.connect('search-changed', self.__handle_search_changed)
+		self._search_query = None
 
 		# create notebook for different lists
-		self._notebook = Gtk.Notebook.new()
-		self._page_index = {}
-		self._page_names = {}
+		list_container = Gtk.ScrolledWindow.new()
+		list_container.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.ALWAYS)
+		list_container.set_size_request(-1, 300)
+		list_container.set_shadow_type(Gtk.ShadowType.IN)
+
+		self._list = Gtk.ListBox.new()
+		self._list.set_header_func(self.__handle_set_header)
+		self._list.set_filter_func(self.__handle_filter_check_location)
 
 		# create button box and commonly used buttons
 		hbox_buttons = Gtk.ButtonBox.new(Gtk.Orientation.HORIZONTAL)
@@ -57,31 +66,17 @@ class LocationMenu:
 		button_open_terminal.set_tooltip_text(_('Open terminal at selected path'))
 		hbox_buttons.pack_start(button_open_terminal, False, False, 0)
 
-		# create bookmarks list
-		bookmarks_container = Gtk.ScrolledWindow.new()
-		bookmarks_container.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.ALWAYS)
-		bookmarks_container.set_size_request(-1, 300)
-
-		self._bookmarks = Gtk.ListBox.new()
-		bookmarks_container.add(self._bookmarks)
-
-		# create mounts list
-		mounts_container = Gtk.ScrolledWindow.new()
-		mounts_container.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.ALWAYS)
-		mounts_container.set_size_request(-1, 300)
-
-		self._mounts = Gtk.ListBox.new()
-		mounts_container.add(self._mounts)
+		# populate headers
+		self.add_header(Bookmark, GenericHeader(_('Bookmarks')))
 
 		# populate lists
 		self.update_bookmarks()
 
 		# pack interface
-		self.add_list('bookmarks', Gtk.Label.new(_('Bookmarks')), bookmarks_container, self._bookmarks)
-		self.add_list('mounts', Gtk.Label.new(_('Mounts')), mounts_container, self._mounts)
+		list_container.add(self._list)
 
 		container.pack_start(self._search_field, True, False, 0)
-		container.pack_start(self._notebook, True, True, 5)
+		container.pack_start(list_container, True, True, 5)
 		container.pack_start(hbox_buttons, True, False, 0)
 
 		container.show_all()
@@ -96,17 +91,15 @@ class LocationMenu:
 		icon_manager = self._application.icon_manager
 
 		# clear existing entries
-		self._bookmarks.foreach(self._bookmarks.remove)
+		self.clear_locations(Bookmark)
 
 		# sunflower bookmarks
-		self._bookmarks.add(GroupTitle(_('User defined')))
-
 		if options.get('add_home'):
-			self._bookmarks.add(Bookmark(os.path.expanduser('~'), 'user-home', _('Home directory')))
+			self.add_location(Bookmark(os.path.expanduser('~'), 'user-home', _('Home directory')))
 
 		for data in options.get('bookmarks'):
 			icon_name = icon_manager.get_icon_for_directory(data['uri'])
-			self._bookmarks.add(Bookmark(data['uri'], icon_name, data['name']))
+			self.add_location(Bookmark(data['uri'], icon_name, data['name']))
 
 		# gnome bookmarks
 		bookmarks_files = (
@@ -116,8 +109,6 @@ class LocationMenu:
 		available_files = [path for path in bookmarks_files if os.path.exists(path)]
 
 		if options.get('system_bookmarks') and len(available_files) > 0:
-			self._bookmarks.add(GroupTitle(_('System wide')))
-
 			lines = []
 			with open(available_files[0], 'r') as raw_file:
 				lines.extend(raw_file.readlines(False))
@@ -134,31 +125,30 @@ class LocationMenu:
 
 				# add entry
 				icon_name = icon_manager.get_icon_for_directory(uri)
-				self._bookmarks.add(Bookmark(uri, icon_name, name))
+				self.add_location(Bookmark(uri, icon_name, name))
 
 	def __get_selected_location(self):
 		"""Return location object selected or None."""
-		list_control = self._page_index[self._notebook.get_current_page()]
-		return list_control.get_selected_row()
+		return self._list.get_selected_row()
 
 	def __handle_open_click(self, widget, data=None):
 		"""Handle clicking on open button."""
 		selected_location = self.__get_selected_location()
-		assert self._control is not None or selected_location is not None
+		assert self._active_object is not None or selected_location is not None
 
 		# close menu
 		self._popover.popdown()
 
 		# open selected path in currently active list
-		if isinstance(self._control, ItemList):
-			self._control.change_path(selected_location.get_location())
+		if isinstance(self._active_object, ItemList):
+			self._active_object.change_path(selected_location.get_location())
 
 		return True
 
 	def __handle_open_tab_click(self, widget, data=None):
 		"""Handle clicking on open in new tab button."""
 		selected_location = self.__get_selected_location()
-		assert self._control is not None or selected_location is not None
+		assert self._active_object is not None or selected_location is not None
 
 		# close menu
 		self._popover.popdown()
@@ -169,20 +159,20 @@ class LocationMenu:
 
 		# create new tab
 		TabClass = self._application.plugin_classes['file_list']
-		self._application.create_tab(self._control._notebook, TabClass, options)
+		self._application.create_tab(self._active_object._notebook, TabClass, options)
 
 		return True
 
 	def __handle_open_opposite_click(self, widget, data=None):
 		"""Handle slicking on open in opposite panel button."""
 		selected_location = self.__get_selected_location()
-		assert self._control is not None or selected_location is not None
+		assert self._active_object is not None or selected_location is not None
 
 		# close menu
 		self._popover.popdown()
 
 		# open in opposite object
-		opposite_object = self._application.get_opposite_object(self._control)
+		opposite_object = self._application.get_opposite_object(self._active_object)
 		if isinstance(opposite_object, ItemList):
 			opposite_object.change_path(selected_location.get_location())
 
@@ -191,7 +181,7 @@ class LocationMenu:
 	def __handle_open_terminal_click(self, widget, data=None):
 		"""Handle clicking on open in terminal button."""
 		selected_location = self.__get_selected_location()
-		assert self._control is not None or selected_location is not None
+		assert self._active_object is not None or selected_location is not None
 
 		# close menu
 		self._popover.popdown()
@@ -202,70 +192,86 @@ class LocationMenu:
 
 		# create new tab
 		TabClass = self._application.plugin_classes['system_terminal']
-		self._application.create_tab(self._control._notebook, TabClass, options)
+		self._application.create_tab(self._active_object._notebook, TabClass, options)
 
 		return True
 
-	def add_list(self, name, title, container, list_control):
-		"""Add list control with specified name to the notebook.
+	def __handle_set_header(self, row, before=None, data=None):
+		"""Set header widget for specified row."""
+		class_name = row.__class__.__name__
 
-		If list with the specified name already exists it will be replaced by
-		new control. This is to allow pane-specific lists. Two widgets are provided
-		through parameters, `container` to be added to the notebook and `list_control`
-		itself which will be used to get active location.
+		# make sure we have header for this class
+		if class_name not in self._headers:
+			return
 
-		"""
-		if name not in self._page_names:
-			# add new page to list
-			index = self._notebook.append_page(container, title)
-			self._page_index[index] = list_control
-			self._page_names[name] = index
+		if class_name in self._assigned and self._assigned[class_name]:
+			return
 
-		else:
-			# replace existing page with new
-			index = self._page_names[name]
-			self._notebook.remove_page(index)
-			self._notebook.insert_page(container, title, index)
-			self._page_index[index] = list_control
+		# set new header
+		row.set_header(self._headers[class_name])
+		self._assigned[class_name] = row
 
-	def get_list(self, name):
-		"""Return list control for the specified name."""
-		result = None
+	def __handle_search_changed(self, widget):
+		"""Handle change in search entry."""
+		for class_name in self._assigned.keys():
+			self._assigned[class_name] = None
 
-		if name in self._page_names:
-			index = self._page_names[name]
-			result = self._page_index[index]
+		self._search_query = widget.get_text().lower()
+		self._list.invalidate_filter()
 
+	def __handle_filter_check_location(self, row, data=None):
+		"""Determine whether location should be visible."""
+		row.set_header(None)
+		return self._search_query in row.get_location().lower() if self._search_query else True
+
+	def add_header(self, Class, widget):
+		"""Add header widget for specified location class name."""
+		self._headers[Class.__name__] = widget
+		self._assigned[Class.__name__] = None
+
+	def add_location(self, widget):
+		"""Add location row. If header for this row class has been specified it will
+		be automatically assigned to the destination group."""
+		self._list.add(widget)
+
+	def remove_location(self, location):
+		"""Remove specified location."""
+		if location in self._assigned.values():
+			self._assigned[location.__class__.__name__] = None
+
+		self._list.remove(location)
+
+	def get_locations(self, Class=None):
+		"""Return list of bookmarks for provided location class."""
+		result = []
 		return result
 
-	def set_current(self, control):
-		"""Set current control to be used as default target for changing path."""
-		self._control = control
+	def clear_locations(self, Class=None):
+		"""Clear all locations for specified location class."""
+		pass
 
-	def show(self, reference):
+	def show(self, reference, active_object=None):
 		"""Show location menu for reference widget."""
 		# update icons
-		if self._application.get_left_object() == self._control:
+		if self._application.get_left_object() == self._active_object:
 			self._button_open_opposite.get_image().set_from_icon_name('go-next-symbolic', Gtk.IconSize.BUTTON)
 		else:
 			self._button_open_opposite.get_image().set_from_icon_name('go-previous-symbolic', Gtk.IconSize.BUTTON)
+
+		# store active object for later use
+		self._active_object = active_object
 
 		# show menu
 		self._popover.set_relative_to(reference)
 		self._popover.popup()
 
 
-class GroupTitle(Gtk.ListBoxRow):
+class GenericHeader(Gtk.VBox):
 	"""Simple group title for locations."""
 
 	def __init__(self, title):
-		Gtk.ListBoxRow.__init__(self)
-		self.set_activatable(False)
-		self.set_selectable(False)
-
-		# generic container
-		container = Gtk.VBox.new(False, 0)
-		container.set_border_width(5)
+		Gtk.VBox.__init__(self)
+		self.set_border_width(5)
 
 		# create title
 		self._title = Gtk.Label.new('<b>{}</b>'.format(title))
@@ -277,12 +283,11 @@ class GroupTitle(Gtk.ListBoxRow):
 		separator = Gtk.Separator.new(Gtk.Orientation.HORIZONTAL)
 
 		# pack user interface
-		container.pack_start(self._title, True, False, 0)
-		container.pack_start(separator, True, False, 0)
-		self.add(container)
+		self.pack_start(self._title, True, False, 0)
+		self.pack_start(separator, True, False, 0)
 
 		# show all components
-		container.show_all()
+		self.show_all()
 
 
 class Location(Gtk.ListBoxRow):

fbbc072432b7c96555b38c5434d7d0705f9d786e
Mladen Mijatov <meaneye.rcf@gmail.com>
2020-02-08 15:21:57 +0100

    Setup: Switch from installing custom script to defining entry point.

--- setup.py
+++ setup.py
@@ -20,19 +20,19 @@ setup(
 		url='https://sunflower-fm.org',
 		license='GPLv3',
 		install_requires=[
-				'gir1.2-gtk-3.0 >= 3.22',
-				'gir1.2-notify >= 0.7',
-				'gir1.2-gdkpixbuf >= 2.0',
-				'gir1.2-vte >= 2.90',
-				'gir1.2-glib >= 2.0',
-				'gir1.2-pango',
+			'gir1.2-gtk-3.0 >= 3.22',
+			'gir1.2-notify >= 0.7',
+			'gir1.2-gdkpixbuf >= 2.0',
+			'gir1.2-vte >= 2.90',
+			'gir1.2-glib >= 2.0',
+			'gir1.2-pango',
 			],
 		packages=find_packages(),
 		include_package_data=True,
 		data_files=[
-				('share/icons/hicolor/scalable/apps', ['images/sunflower.svg']),
-				('share/pixmaps/sunflower', ['images/splash.png']),
-				('share/applications', ['Sunflower.desktop'])
+			('share/icons/hicolor/scalable/apps', ['images/sunflower.svg']),
+			('share/pixmaps/sunflower', ['images/splash.png']),
+			('share/applications', ['Sunflower.desktop'])
 			],
-		scripts=['dist/sunflower']
+		entry_points={'console_scripts': ['sunflower = sunflower.__main__:main']}
 		)
